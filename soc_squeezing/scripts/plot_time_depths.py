d4c4313b9b4716280b85116764f256ea2123cc6e
Michael A. Perlin <mika.perlin@gmail.com>
2018-07-30 15:19:12 -0500

    plot optimal OAT squeezing time in (V_0,V_T) plane

--- soc_squeezing/scripts/plot_time_depths.py
+++ soc_squeezing/scripts/plot_time_depths.py
@@ -9,13 +9,14 @@ import matplotlib.patches as mpatches
 from scipy.optimize import minimize_scalar
 
 from dicke_methods import squeezing_OAT
-from fermi_hubbard_methods import get_simulation_parameters, spatial_basis, gauged_energy
+from fermi_hubbard_methods import get_simulation_parameters
 from sr87_olc_constants import g_int_LU, recoil_energy_NU
 
 show = "show" in sys.argv
 save = "save" in sys.argv
 assert(show or save)
 
+dpi = 600
 figsize = (4.2,3)
 params = { "text.usetex" : True }
 plt.rcParams.update(params)
@@ -26,26 +27,23 @@ fig_dir = "../figures/"
 sites_1D = 100 # number of lattice sites along each axis of the lattice
 lattice_dimensions = [ 1, 2 ] # dimensions of lattice
 
-# set spin-orbit coupling strength
-soc_int = 2
-phi = 2*np.pi/sites_1D * soc_int
-
 # min / max primary lattice depths
 depth_min = 1
-depth_max = 10
+depth_max = 20
 depth_step_size = 0.1
 
 # min / max confinement lattice depths
 confinement_min = 40
-confinement_max = 100
-confinement_step_size = 2
+confinement_max = 200
+confinement_step_size = 1
 
-# squeezing viability bounds
-t_opt_SI_cap = 2
-h_std_U_cap = 0.05
+# squeezing protocol requirements
+h_std_U_target = 0.05
 t_J_T_cap = 0.05
+U_J_cap = 10
+t_opt_SI_cap = 1
 
-site_number = 400 # site number for lattice calculations
+max_tau = 2 # maximum value of reduced time in OAT squeezing minimization
 
 depths = np.arange(depth_min,
                    depth_max + depth_step_size/2,
@@ -56,37 +54,39 @@ confinements = np.arange(confinement_min,
 
 
 ##########################################################################################
-# method to compute parameters which determine the viability of a squeezing protocol
+# method to compute relevant parameters in an optimal squeezing protocol
 ##########################################################################################
 
-def get_single_lattice_parameters(L, depth, confinement):
-    L, J_0, _, K_0, _, _, energies, J_T, K_T = \
-        get_simulation_parameters(L, phi, depth, confinement, site_number)
+def get_single_optimal_parameters(L, depth, confinement):
+    L, J_0, _, K_0, _, _, _, J_T, K_T = \
+        get_simulation_parameters(L, 0, depth, confinement, sites_1D)
 
     N = np.prod(L)
     U_int = g_int_LU[1] * K_T**(3-L.size) * np.prod(K_0)
-    h_std = np.std([ ( gauged_energy(q, 1, phi, L, energies)
-                         - gauged_energy(q, 0, phi, L, energies) )
-                       for q in spatial_basis(L) ])
+
+    def h_std(phi): return 2**(1+L.size/2)*J_0[0]*np.sin(phi/2)
+    phi_opt = minimize_scalar(lambda x: abs(h_std(x)/U_int-h_std_U_target),
+                              method = "bounded", bounds = (0, np.pi)).x
 
     def squeezing_OAT_val(tau):
         chi_t = tau * N**(-2/3)
         return squeezing_OAT(chi_t, N)
-    optimum = minimize_scalar(squeezing_OAT_val, method = "bounded", bounds = (0, 2))
+    tau_opt = minimize_scalar(squeezing_OAT_val,
+                              method = "bounded", bounds = (0, max_tau)).x
 
-    chi = h_std**2 / ( N * (N-1) * U_int / np.prod(L) )
-    time_opt = optimum.x * N **(-2/3) / chi
+    chi = h_std(phi_opt)**2 / ( N * (N-1) * U_int / np.prod(L) )
+    t_opt = tau_opt * N **(-2/3) / chi
 
-    return h_std, U_int, time_opt, J_0[0], J_T
+    return U_int, phi_opt, t_opt, J_0[0], J_T
 
-def get_lattice_parameters(lattice_dim):
+def get_optimal_parameters(lattice_dim):
     L = sites_1D * np.ones(lattice_dim)
     zero_frame = pd.DataFrame(data = np.zeros((len(depths),len(confinements))),
                               index = depths, columns = confinements)
 
-    h_std = zero_frame.copy(deep = True)
     U_int = zero_frame.copy(deep = True)
-    time_opt = zero_frame.copy(deep = True)
+    phi_opt = zero_frame.copy(deep = True)
+    t_opt = zero_frame.copy(deep = True)
     J_0 = pd.DataFrame(data = np.zeros(len(depths)), index = depths)
     J_T = pd.DataFrame(data = np.zeros(len(confinements)), index = confinements)
 
@@ -96,18 +96,18 @@ def get_lattice_parameters(lattice_dim):
         print(" depth: {}/{}".format(dd,dd_cap))
         dd += 1
         for confinement in confinements:
-            params = get_single_lattice_parameters(L, depth, confinement)
-            h_std.at[depth, confinement] = params[0]
-            U_int.at[depth, confinement] = params[1]
-            time_opt.at[depth, confinement] = params[2]
+            params = get_single_squeezing_parameters(L, depth, confinement)
+            U_int.at[depth, confinement] = params[0]
+            phi_opt.at[depth, confinement] = params[1]
+            t_opt.at[depth, confinement] = params[2]
             J_0.at[depth] = params[3]
             J_T.at[confinement] = params[4]
 
-    return h_std, U_int, time_opt, J_0, J_T
+    return U_int, phi_opt, t_opt, J_0, J_T
 
 
 ##########################################################################################
-# method to make a color plot summarizng viability of the squeezing protocol
+# method to make a plot of optimal squeezing times in the (V_0,V_T) plane
 ##########################################################################################
 
 x_min, x_max, dx = depth_min, depth_max, depth_step_size
@@ -116,61 +116,63 @@ x_min, x_max = x_min - dx/2, x_max + dx/2
 y_min, y_max = y_min - dy/2, y_max + dy/2
 axis_bounds = [ x_min, x_max, y_min, y_max ]
 axis_aspect = ( x_max - x_min ) / ( y_max - y_min ) * figsize[1]/figsize[0]
-def make_plot(h_std, U_int, t_opt, J_T, time_ratio = 1):
-    h_std_U = h_std / U_int
+def make_plot(U_int, phi_opt, t_opt, J_0, J_T, time_ratio = 1):
     t_opt = t_opt * time_ratio
     t_opt_SI = t_opt / recoil_energy_NU
 
-    t_shade_vals = np.zeros(h_std.shape)
-    tJ_hatch_vals = np.zeros(h_std.shape)
-    h_hatch_vals = np.zeros(h_std.shape)
+    t_J_shade_vals = np.ones(U_int.shape)
+    U_J_shade_vals = np.ones(U_int.shape)
 
-    t_shade_vals[t_opt_SI.values < t_opt_SI_cap] = 1
-    h_hatch_vals[h_std_U.values < h_std_U_cap] = 1
-    tJ_hatch_vals[t_opt.values * J_T.values.T / (2*np.pi) < t_J_T_cap] = 1
+    t_J_shade_vals[t_opt.values * J_T.values.T / (2*np.pi) > t_J_T_cap] = 0
+    U_J_shade_vals[(U_int.values.T / J_0.values.T).T > 10] = 0
 
-    depths = h_std_U.index.values
-    confinements = pd.to_numeric(h_std_U.columns).values
+    depths = U_int.index.values
+    confinements = pd.to_numeric(U_int.columns).values
 
     plt.figure(figsize = figsize)
 
+    vmax = min(t_opt_SI.values.max(), t_opt_SI_cap)
+    plt.pcolormesh(depths, confinements, t_opt_SI.T, vmax = vmax, zorder = 0)
+    plt.colorbar(label = r"$t_{\mathrm{opt}}$ (seconds)")
+
     contour_level = [ 0.5 ]
-    plt.contour(depths, confinements, t_shade_vals.T, contour_level,
-                colors = "black", linewidths = (2,),)
-    plt.contour(depths, confinements, h_hatch_vals.T, contour_level,
-                colors = "black", linewidths = (2,))
-    plt.contour(depths, confinements, tJ_hatch_vals.T, contour_level,
-                colors = "black", linewidths = (2,))
-
     fill_regions = 2
-    plt.contourf(depths, confinements, -t_shade_vals.T,
-                 cmap = plt.get_cmap("binary"), vmin = -1, vmax = 1)
-    plt.contourf(depths, confinements, -h_hatch_vals.T, fill_regions,
-                 colors = "none", hatches = [ None, None, "/" ])
-    plt.contourf(depths, confinements, -tJ_hatch_vals.T, fill_regions,
-                 colors = "none", hatches = [ None, None, "\\" ])
-
-    colors = [ "#888888", "#FFFFFF", "#FFFFFF" ]
-    labels = [ r"$t_{{\mathrm{{opt}}}} > {}$ seconds".format(t_opt_SI_cap),
-               r"$\tilde h/U > {}$".format(h_std_U_cap),
-               r"$t_{{\mathrm{{opt}}}} J_T/2\pi > {}$".format(t_J_T_cap) ]
-    hatches = [ None, "///", "\\\\\\" ]
-
+    colors = []
+    labels = []
+    hatches = []
+    if 0 in t_J_shade_vals:
+        plt.contour(depths, confinements, t_J_shade_vals.T, contour_level,
+                    colors = "black", linewidths = (2,), zorder = 1)
+        plt.contourf(depths, confinements, t_J_shade_vals.T, fill_regions,
+                 colors = "none", hatches = [ "\\", None, None ], zorder = 1)
+        colors.append("#FFFFFF")
+        labels.append(r"$t_{{\mathrm{{opt}}}} J_T/2\pi > {}$".format(t_J_T_cap))
+        hatches.append("\\\\\\")
+    if 0 in U_J_shade_vals:
+        plt.contour(depths, confinements, U_J_shade_vals.T, contour_level,
+                    colors = "black", linewidths = (2,), zorder = 1)
+        plt.contourf(depths, confinements, U_J_shade_vals.T, fill_regions,
+                     colors = "none", hatches = [ "/", None, None ], zorder = 1)
+        colors.append("#FFFFFF")
+        labels.append(r"$U_{{\mathrm{{int}}}}/J_0 > {}$".format(U_J_cap))
+        hatches.append("///")
     handles = [ mpatches.Rectangle([0,0], 0, 0, edgecolor = "black",
                                    facecolor = colors[ii],
                                    hatch = hatches[ii],
                                    label = labels[ii])
-                for ii in range(3) ]
+                for ii in range(len(colors)) ]
+    if len(handles) > 0:
+        legend = plt.legend(handles = handles, ncol = len(handles), framealpha = 0,
+                            loc = "upper center", bbox_to_anchor = (0.5,1.2))
+        legend.set_zorder(1)
 
-    plt.legend(handles = handles, framealpha = 1, loc = "lower right")
-
-    plt.xlabel(r"$V_0/E_R$")
-    plt.ylabel(r"$V_T/E_R$")
-    plt.tight_layout()
+    plt.xlabel(r"$V_0/E_R$", zorder = 1)
+    plt.ylabel(r"$V_T/E_R$", zorder = 1)
+    plt.tight_layout(pad = 0.5)
 
 
 ##########################################################################################
-# compute lattice parameters and make squeezing viability plots
+# compute optimal squeezing protocol parameters and make optimal squeezing time plots
 ##########################################################################################
 
 def pd_read_1D(fname):
@@ -190,27 +192,27 @@ header_1D = "# first column = lattice depth\n"
 header_units = "# values in units with the recoil energy"
 header_units += r" E_R \approx 3.47 x 2\pi kHz equal to 1" + "\n"
 
-# loop over all lattice dimensions and make corresponding viability figures
+# loop over all lattice dimensions
 for lattice_dim in lattice_dimensions:
 
     # set data file names and header identifying this simulation
-    fname_suffix = "_L{}_soc{}_{}D".format(sites_1D,soc_int,lattice_dim)
+    fname_suffix = "_L{}_{}D".format(sites_1D,lattice_dim)
     base_fname = data_dir + "{}" + fname_suffix + ".txt"
-    h_std_fname = base_fname.format("h_std")
     U_int_fname = base_fname.format("U_int")
+    phi_opt_fname = base_fname.format("phi_opt")
     t_opt_fname = base_fname.format("t_opt")
     J_0_fname = data_dir + "J_0.txt"
     J_T_fname = data_dir + "J_T.txt"
 
-    # determine whether we need to compute lattice parameters,
+    # determine whether we need to compute optimal protocol parameters,
     #   or whether we can simply read in a data file
     compute_lattice_params = False
-    if not os.path.isfile(h_std_fname):
+    if not os.path.isfile(U_int_fname):
         compute_lattice_params = True
     else:
-        h_std = pd.read_csv(h_std_fname, comment = "#", header = 0, index_col = 0)
-        f_depths = h_std.index.values
-        f_confinements = pd.to_numeric(h_std.columns).values
+        U_int = pd.read_csv(U_int_fname, comment = "#", header = 0, index_col = 0)
+        f_depths = U_int.index.values
+        f_confinements = pd.to_numeric(U_int.columns).values
         depth_test = ( f_depths[0] <= depths[0] and
                        f_depths[-1] >= depths[-1] and
                        f_depths.size >= depths.size )
@@ -220,13 +222,13 @@ for lattice_dim in lattice_dimensions:
         if not depth_test or not confinement_test:
             compute_lattice_params = True
 
-    # get viability parameters, writing them to a file if we compute them
+    # get optimal parameters, writing them to a file if we compute them
     if compute_lattice_params:
-        h_std, U_int, t_opt, J_0, J_T = get_lattice_parameters(lattice_dim)
+        U_int, phi_opt, t_opt, J_0, J_T = get_optimal_parameters(lattice_dim)
 
         if save:
-            for frame_2D, name_2D in zip([ h_std, U_int, t_opt ],
-                                         [ h_std_fname, U_int_fname, t_opt_fname ]):
+            for frame_2D, name_2D in zip([ U_int, phi_opt, t_opt ],
+                                         [ U_int_fname, phi_opt_fname, t_opt_fname ]):
                 with open(name_2D, "w") as f:
                     f.write(header_2D + header_units)
                 frame_2D.to_csv(name_2D, mode = "a")
@@ -237,24 +239,29 @@ for lattice_dim in lattice_dimensions:
                 frame_1D.to_csv(name_1D, header = False, mode = "a")
 
     else:
-        h_std = pd_read_2D(h_std_fname)
         U_int = pd_read_2D(U_int_fname)
+        phi_opt = pd_read_2D(phi_opt_fname)
         t_opt = pd_read_2D(t_opt_fname)
+        J_0 = pd_read_1D(J_0_fname)
         J_T = pd_read_1D(J_T_fname)
 
-    # make and save OAT viability plot
-    make_plot(h_std, U_int, t_opt, J_T)
+    # make and save OAT time plot
+    make_plot(U_int, phi_opt, t_opt, J_0, J_T)
     if save:
-        plt.savefig(fig_dir + "viability_OAT" + fname_suffix + ".pdf")
+        plt.gca().set_rasterization_zorder(1)
+        plt.savefig(fig_dir + "t_opt_OAT" + fname_suffix + ".pdf",
+                    rasterized = True, dpi = dpi)
 
     # determine TAT : OAT optimal squeezing time ratio
     N = sites_1D**lattice_dim
     ratio_index = time_ratios.index.get_loc(N, method = "nearest")
     time_ratio = time_ratios.iat[ratio_index]
 
-    # make and save TAT viability plot
-    make_plot(h_std, U_int, t_opt, J_T, time_ratio)
+    # make and save TAT time plot
+    make_plot(U_int, phi_opt, t_opt, J_0, J_T, time_ratio)
     if save:
-        plt.savefig(fig_dir + "viability_TAT" + fname_suffix + ".pdf")
+        plt.gca().set_rasterization_zorder(1)
+        plt.savefig(fig_dir + "t_opt_TAT" + fname_suffix + ".pdf",
+                    rasterized = True, dpi = dpi)
 
 if show: plt.show()
