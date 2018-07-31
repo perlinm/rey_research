541626341899428c7ebf21658a92550275539284
Michael A. Perlin <mika.perlin@gmail.com>
2018-07-31 10:09:54 -0500

    compute tunneling rates in separate file

--- soc_squeezing/scripts/plot_time_depths.py
+++ soc_squeezing/scripts/plot_time_depths.py
@@ -27,16 +27,6 @@ fig_dir = "../figures/"
 size_1D = int(sys.argv[1]) # lattice size (i.e. number of lattice sites along each axis)
 lattice_dim = int(sys.argv[2]) # dimensions of lattice
 
-# min / max primary lattice depths
-depth_min = 1
-depth_max = 20
-depth_step_size = 0.1
-
-# min / max confinement lattice depths
-confinement_min = 40
-confinement_max = 200
-confinement_step_size = 1
-
 # squeezing protocol requirements
 h_U_target = 0.05
 t_J_T_cap = 0.05
@@ -45,13 +35,19 @@ t_opt_SI_cap = 1
 
 max_tau = 2 # maximum value of reduced time in OAT squeezing minimization
 
-L = size_1D * np.ones(lattice_dim)
-depths = np.arange(depth_min,
-                   depth_max + depth_step_size/2,
-                   depth_step_size)
-confinements = np.arange(confinement_min,
-                         confinement_max + confinement_step_size/2,
-                         confinement_step_size)
+# methods to read in 1D / 2D data
+def pd_read_1D(fname):
+    return pd.read_csv(fname, comment = "#", squeeze = True, header = None, index_col = 0)
+def pd_read_2D(fname):
+    return pd.read_csv(fname, comment = "#", header = 0, index_col = 0)
+
+# primary and transverse tunneling rates
+J_0 = pd_read_1D(data_dir + "J_0.txt")
+J_T = pd_read_1D(data_dir + "J_T.txt")
+
+# primary and transverse lattice depths
+depths = J_0.index
+confinements = J_T.index
 
 
 ##########################################################################################
@@ -59,7 +55,8 @@ confinements = np.arange(confinement_min,
 ##########################################################################################
 
 def get_single_optimum_parameters(depth, confinement):
-    _, J_0, J_T, K_0, K_T, _, _, _ = \
+    L = size_1D * np.ones(lattice_dim)
+    _, _, _, K_0, K_T, _, _, _ = \
         get_simulation_parameters(L, depth, confinement, size_1D)
 
     N = np.prod(L)
@@ -78,7 +75,7 @@ def get_single_optimum_parameters(depth, confinement):
     chi = h_std(phi_opt)**2 / ( N * (N-1) * U_int / np.prod(L) )
     t_opt = tau_opt * N **(-2/3) / chi
 
-    return U_int, phi_opt, t_opt, J_0[0], J_T
+    return U_int, phi_opt, t_opt
 
 def get_optimum_parameters():
     zero_frame = pd.DataFrame(data = np.zeros((len(depths),len(confinements))),
@@ -87,8 +84,6 @@ def get_optimum_parameters():
     U_int = zero_frame.copy(deep = True)
     phi_opt = zero_frame.copy(deep = True)
     t_opt = zero_frame.copy(deep = True)
-    J_0 = pd.DataFrame(data = np.zeros(len(depths)), index = depths)
-    J_T = pd.DataFrame(data = np.zeros(len(confinements)), index = confinements)
 
     dd, dd_cap = 0, len(depths)
     for depth in depths:
@@ -99,23 +94,22 @@ def get_optimum_parameters():
             U_int.at[depth, confinement] = params[0]
             phi_opt.at[depth, confinement] = params[1]
             t_opt.at[depth, confinement] = params[2]
-            J_0.at[depth] = params[3]
-            J_T.at[confinement] = params[4]
 
-    return U_int, phi_opt, t_opt, J_0, J_T
+    return U_int, phi_opt, t_opt
 
 
 ##########################################################################################
 # method to make a plot of optimal squeezing times in the (V_0,V_T) plane
 ##########################################################################################
 
-x_min, x_max, dx = depth_min, depth_max, depth_step_size
-y_min, y_max, dy = confinement_min, confinement_max, confinement_step_size
+x_min, x_max, n_x = depths[0], depths[-1], len(depths)
+y_min, y_max, n_y = confinements[0], confinements[-1], len(confinements)
+dx, dy = ( x_max - x_min ) / n_x, ( y_max - y_min ) / n_y
 x_min, x_max = x_min - dx/2, x_max + dx/2
 y_min, y_max = y_min - dy/2, y_max + dy/2
 axis_bounds = [ x_min, x_max, y_min, y_max ]
 axis_aspect = ( x_max - x_min ) / ( y_max - y_min ) * figsize[1]/figsize[0]
-def make_plot(U_int, phi_opt, t_opt, J_0, J_T, time_ratio = 1):
+def make_plot(U_int, phi_opt, t_opt, time_ratio = 1):
     t_opt = t_opt * time_ratio
     t_opt_SI = t_opt / recoil_energy_NU
 
@@ -175,11 +169,6 @@ def make_plot(U_int, phi_opt, t_opt, J_0, J_T, time_ratio = 1):
 # compute optimal squeezing protocol parameters and make optimal squeezing time plots
 ##########################################################################################
 
-def pd_read_1D(fname):
-    return pd.read_csv(fname, comment = "#", squeeze = True, header = None, index_col = 0)
-def pd_read_2D(fname):
-    return pd.read_csv(fname, comment = "#", header = 0, index_col = 0)
-
 time_OAT_fname = data_dir + "time_OAT.txt"
 time_TAT_fname = data_dir + "time_TAT.txt"
 time_OAT_vals = pd_read_1D(time_OAT_fname)
@@ -198,52 +187,25 @@ base_fname = data_dir + "{}" + fname_suffix + ".txt"
 U_int_fname = base_fname.format("U_int")
 phi_opt_fname = base_fname.format("phi_opt")
 t_opt_fname = base_fname.format("t_opt")
-J_0_fname = data_dir + "J_0.txt"
-J_T_fname = data_dir + "J_T.txt"
-
-# determine whether we need to compute optimal protocol parameters,
-#   or whether we can simply read in a data file
-compute_lattice_params = False
-if not os.path.isfile(U_int_fname):
-    compute_lattice_params = True
-else:
-    U_int = pd.read_csv(U_int_fname, comment = "#", header = 0, index_col = 0)
-    f_depths = U_int.index.values
-    f_confinements = pd.to_numeric(U_int.columns).values
-    depth_test = ( f_depths[0] == depths[0] and
-                   f_depths[-1] == depths[-1] and
-                   f_depths.size == depths.size )
-    confinement_test = ( f_confinements[0] == confinements[0] and
-                         f_confinements[-1] == confinements[-1] and
-                         f_confinements.size == confinements.size )
-    if not depth_test or not confinement_test:
-        compute_lattice_params = True
 
 # get optimal parameters, writing them to a file if we compute them
-if compute_lattice_params:
-    U_int, phi_opt, t_opt, J_0, J_T = get_optimum_parameters()
+if not os.path.isfile(U_int_fname):
+    U_int, phi_opt, t_opt = get_optimum_parameters()
 
     if save:
-        for frame_2D, name_2D in zip([ U_int, phi_opt, t_opt ],
-                                     [ U_int_fname, phi_opt_fname, t_opt_fname ]):
-            with open(name_2D, "w") as f:
+        for data, fname in zip([ U_int, phi_opt, t_opt ],
+                               [ U_int_fname, phi_opt_fname, t_opt_fname ]):
+            with open(fname, "w") as f:
                 f.write(header_2D + header_units)
-            frame_2D.to_csv(name_2D, mode = "a")
-
-        for frame_1D, name_1D in zip([ J_0, J_T ], [ J_0_fname, J_T_fname ]):
-            with open(name_1D, "w") as f:
-                f.write(header_1D + header_units)
-            frame_1D.to_csv(name_1D, header = False, mode = "a")
+            data.to_csv(fname, mode = "a")
 
 else:
     U_int = pd_read_2D(U_int_fname)
     phi_opt = pd_read_2D(phi_opt_fname)
     t_opt = pd_read_2D(t_opt_fname)
-    J_0 = pd_read_1D(J_0_fname)
-    J_T = pd_read_1D(J_T_fname)
 
 # make and save OAT time plot
-make_plot(U_int, phi_opt, t_opt, J_0, J_T)
+make_plot(U_int, phi_opt, t_opt)
 if save:
     plt.gca().set_rasterization_zorder(1)
     plt.savefig(fig_dir + "t_opt_OAT" + fname_suffix + ".pdf",
@@ -255,7 +217,7 @@ ratio_index = time_ratios.index.get_loc(N, method = "nearest")
 time_ratio = time_ratios.iat[ratio_index]
 
 # make and save TAT time plot
-make_plot(U_int, phi_opt, t_opt, J_0, J_T, time_ratio)
+make_plot(U_int, phi_opt, t_opt, time_ratio)
 if save:
     plt.gca().set_rasterization_zorder(1)
     plt.savefig(fig_dir + "t_opt_TAT" + fname_suffix + ".pdf",
