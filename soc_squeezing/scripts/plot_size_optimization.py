6572dcee41234db4f3dfc5c6a1ae704bf7f7823a
Michael A. Perlin <mika.perlin@gmail.com>
2018-08-05 15:06:33 -0500

    make 2D squeezing plots in additional to time plots

--- soc_squeezing/scripts/plot_size_optimization.py
+++ soc_squeezing/scripts/plot_size_optimization.py
@@ -29,10 +29,12 @@ params = { "text.usetex" : True }
 plt.rcParams.update(params)
 
 data_dir = "../data/"
-fig_dir = "../figures/optimal_time_plots/"
+fig_dir = "../figures/optimization_plots/"
 
 lattice_dim = int(sys.argv[1]) # dimensions of lattice
 confinement = 200 # # confinement lattice depth
+
+sizes_1D = np.arange(10,101,1).astype(int) # default lattice sizes
 
 # squeezing protocol requirements
 h_U_target = 0.05
@@ -42,14 +44,19 @@ U_J_caps = [ 10, 15, 20 ]
 t_J_hatch = "|"
 U_J_hatches = [ "/", "\\", "-" ]
 
+sqz_dB_floor = 5
+sqz_dB_cap = 11
+
+t_SI_floor = 0
+t_SI_cap = 0.4
+
 depth_min = 2
 if lattice_dim == 1:
     depth_max = 8
-    t_opt_SI_cap = 0.16
 elif lattice_dim == 2:
     depth_max = 10
-    t_opt_SI_cap = 1
 
+excited_lifetime_SI = 10 # seconds; lifetime of excited state (from e --> g decay)
 max_tau = 2 # maximum value of reduced time in OAT squeezing minimization
 
 # methods to read in 1D / 2D data
@@ -66,6 +73,12 @@ J_T = pd_read_1D(data_dir + "J_T.txt")
 depths = J_0.index
 confinements = J_T.index
 
+# convert value to decibels (dB)
+def to_dB(x): return 10*np.log10(x)
+
+# decay rate in lattice units
+decay_rate = 1/excited_lifetime_SI / recoil_energy_NU
+
 
 ##########################################################################################
 # method to compute relevant parameters in an optimal squeezing protocol
@@ -73,26 +86,28 @@ confinements = J_T.index
 
 def get_single_optimum_parameters(depth, size_1D):
     L = size_1D * np.ones(lattice_dim)
-    _, _, _, K_0, K_T, _, _, _ = \
+    _, J_0_here, _, K_0, K_T, _, _, _ = \
         get_simulation_parameters(L, depth, confinement, size_1D)
 
     N = np.prod(L)
     U_int = g_int_LU[1] * K_T**(3-lattice_dim) * np.prod(K_0)
 
-    def h_std(phi): return 2**(1+lattice_dim/2)*J_0[0]*np.sin(phi/2)
+    def h_std(phi): return 2**(1+lattice_dim/2)*J_0_here[0]*np.sin(phi/2)
     phi_opt = minimize_scalar(lambda x: abs(h_std(x)/U_int-h_U_target),
                               method = "bounded", bounds = (0, np.pi)).x
+    chi = h_std(phi_opt)**2 / ( N * (N-1) * U_int / np.prod(L) )
 
     def squeezing_OAT_val(tau):
         chi_t = tau * N**(-2/3)
-        return squeezing_OAT(chi_t, N)
-    tau_opt = minimize_scalar(squeezing_OAT_val,
-                              method = "bounded", bounds = (0, max_tau)).x
+        return squeezing_OAT(chi_t, N, decay_rate / chi)
+    optimum = minimize_scalar(squeezing_OAT_val,
+                              method = "bounded", bounds = (0, max_tau))
+    tau_opt = optimum.x
+    sqz_opt = optimum.fun
 
-    chi = h_std(phi_opt)**2 / ( N * (N-1) * U_int / np.prod(L) )
     t_opt = tau_opt * N **(-2/3) / chi
 
-    return U_int, phi_opt, t_opt
+    return U_int, phi_opt, sqz_opt, t_opt
 
 def get_optimum_parameters():
     zero_frame = pd.DataFrame(data = np.zeros((len(depths),len(sizes_1D))),
@@ -100,6 +115,7 @@ def get_optimum_parameters():
 
     U_int = zero_frame.copy(deep = True)
     phi_opt = zero_frame.copy(deep = True)
+    sqz_opt = zero_frame.copy(deep = True)
     t_opt = zero_frame.copy(deep = True)
 
     dd, dd_cap = 0, len(depths)
@@ -110,9 +126,10 @@ def get_optimum_parameters():
             params = get_single_optimum_parameters(depth, size_1D)
             U_int.at[depth, size_1D] = params[0]
             phi_opt.at[depth, size_1D] = params[1]
-            t_opt.at[depth, size_1D] = params[2]
+            sqz_opt.at[depth, size_1D] = params[2]
+            t_opt.at[depth, size_1D] = params[3]
 
-    return U_int, phi_opt, t_opt
+    return U_int, phi_opt, sqz_opt, t_opt
 
 
 ##########################################################################################
@@ -126,18 +143,7 @@ x_min, x_max = x_min - dx/2, x_max + dx/2
 y_min, y_max = y_min - dy/2, y_max + dy/2
 axis_bounds = [ x_min, x_max, y_min, y_max ]
 axis_aspect = ( x_max - x_min ) / ( y_max - y_min ) * figsize[1]/figsize[0]
-def make_plot(U_int, phi_opt, t_opt, time_ratios = None):
-    N_vals = pd.to_numeric(t_opt.columns)**lattice_dim
-    if type(time_ratios) != type(None):
-        ratios = pd.Series(index = N_vals)
-        ratios.fillna(time_ratios, inplace = True)
-        ratios.rename(np.log, inplace = True)
-        ratios.interpolate(limit_direction = "both", inplace = True)
-        ratios.rename(np.exp, inplace = True)
-        N_ratios = ratios.values
-    else:
-        N_ratios = np.ones(len(N_vals))
-    t_opt_SI = t_opt.values * N_ratios / recoil_energy_NU
+def make_plot(U_int, phi_opt, values, label, vmin = None, vmax = None):
     U_J_vals = (U_int.values.T / J_0.values.T).T
 
     depths = U_int.index.values
@@ -145,9 +151,13 @@ def make_plot(U_int, phi_opt, t_opt, time_ratios = None):
 
     plt.figure(figsize = figsize)
 
-    plt.pcolormesh(depths, sizes_1D, t_opt_SI.T, vmin = 0, vmax = t_opt_SI_cap,
+    if vmin == None:
+        vmin = values[(depths > depth_min) & (depths < depth_max)].min()
+    if vmax == None:
+        vmax = values[(depths > depth_min) & (depths < depth_max)].max()
+    plt.pcolormesh(depths, sizes_1D, values.T, vmin = vmin, vmax = vmax,
                    cmap = plt.get_cmap("jet"), zorder = 0)
-    plt.colorbar(label = r"$t_{\mathrm{opt}}$ (seconds)")
+    plt.colorbar(label = label)
 
     contour_level = [ 0.5 ]
     fill_regions = 2
@@ -184,12 +194,6 @@ def make_plot(U_int, phi_opt, t_opt, time_ratios = None):
 # compute optimal squeezing protocol parameters and make optimal squeezing time plots
 ##########################################################################################
 
-time_OAT_fname = data_dir + "time_OAT.txt"
-time_TAT_fname = data_dir + "time_TAT.txt"
-time_OAT_vals = pd_read_1D(time_OAT_fname)
-time_TAT_vals = pd_read_1D(time_TAT_fname)
-time_ratios = time_TAT_vals / time_OAT_vals
-
 header_2D = "# first column = primary lattice depths\n"
 header_2D += "# first row = confining lattice depths\n"
 header_1D = "# first column = lattice depth\n"
@@ -201,36 +205,40 @@ fname_suffix = "_{}D_T{}".format(lattice_dim,confinement)
 base_fname = data_dir + "{}" + fname_suffix + ".txt"
 U_int_fname = base_fname.format("U_int")
 phi_opt_fname = base_fname.format("phi_opt")
+sqz_opt_fname = base_fname.format("sqz_opt")
 t_opt_fname = base_fname.format("t_opt")
 
 # get optimal parameters, writing them to a file if we compute them
 if not os.path.isfile(U_int_fname):
-    U_int, phi_opt, t_opt = get_optimum_parameters()
+    U_int, phi_opt, sqz_opt, t_opt = get_optimum_parameters()
 
     if save:
-        for data, fname in zip([ U_int, phi_opt, t_opt ],
-                               [ U_int_fname, phi_opt_fname, t_opt_fname ]):
+        for data, fname in [ [ U_int, U_int_fname ],
+                             [ phi_opt, phi_opt_fname ],
+                             [ sqz_opt, sqz_opt_fname ],
+                             [ t_opt, t_opt_fname ] ]:
             with open(fname, "w") as f:
                 f.write(header_2D + header_units)
             data.to_csv(fname, mode = "a")
-
 else:
     U_int = pd_read_2D(U_int_fname)
     phi_opt = pd_read_2D(phi_opt_fname)
+    sqz_opt = pd_read_2D(sqz_opt_fname)
     t_opt = pd_read_2D(t_opt_fname)
 
-# make and save OAT time plot
-make_plot(U_int, phi_opt, t_opt)
-if save:
-    plt.gca().set_rasterization_zorder(1)
-    plt.savefig(fig_dir + "t_opt" + fname_suffix + "_OAT.pdf",
-                rasterized = True, dpi = dpi)
+sqz_opt_dB = -to_dB(sqz_opt.values)
+sqz_opt_list = [ sqz_opt_dB, "sqz_opt", r"$-10\log_{{10}}\xi^2$",
+                 sqz_dB_floor, sqz_dB_cap ]
 
-# make and save TAT time plot
-make_plot(U_int, phi_opt, t_opt, time_ratios)
-if save:
-    plt.gca().set_rasterization_zorder(1)
-    plt.savefig(fig_dir + "t_opt" + fname_suffix + "_TAT.pdf",
-                rasterized = True, dpi = dpi)
+t_opt_SI = t_opt.values / recoil_energy_NU
+t_opt_list = [ t_opt_SI, "t_opt", r"$t_{\mathrm{opt}}$ (seconds)",
+               t_SI_floor, t_SI_cap ]
+
+for values, name, label, floor, cap in [ sqz_opt_list, t_opt_list ]:
+    make_plot(U_int, phi_opt, values, label, floor, cap)
+    if save:
+        plt.gca().set_rasterization_zorder(1)
+        plt.savefig(fig_dir + name + fname_suffix + ".pdf",
+                    rasterized = True, dpi = dpi)
 
 if show: plt.show()
