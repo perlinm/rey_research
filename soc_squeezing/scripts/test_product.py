acbbc394551b006f80ea9d6dd7b6797e67f46936
Michael A. Perlin <mika.perlin@gmail.com>
2018-10-01 00:42:20 -0600

    added code testing latest changes to analytical work on product of two operators

--- soc_squeezing/scripts/test_product.py
+++ soc_squeezing/scripts/test_product.py
@@ -41,6 +41,18 @@ def spin_op(mu):
     if mu == 2: return sm
     if mu == 3: return I2
 
+def poch(nn, kk, vals = {}):
+    return np.prod([ nn - cc for cc in range(kk) ])
+
+def f(mm,nn,rr):
+    mnb_fac = np.prod([ poch(mm[mu],rr[mu,:].sum()) * poch(nn[mu],rr[:,mu].sum())
+                       for mu in range(3) ])
+    rr_fac = np.prod([ factorial(rr[mu,nu]) for mu in range(3) for nu in range(3) ])
+    return mnb_fac / rr_fac
+
+def g(cc,rr_mu_nu):
+    return factorial(rr_mu_nu) / np.prod([ factorial(cc_jj) for cc_jj in cc  ])
+
 def eta(mu, nu, rho, vals = {}):
     try: return vals[mu,nu,rho]
     except:
@@ -56,29 +68,47 @@ def PP(jj,mm):
             op_list[jj_mu] = spin_op(mu)
     return qt.tensor(op_list)
 
-def QQ(jj,rr):
-    jj_vals = partition(jj,rr)
-    op_list = [ I2 ] * N
+def KK_cc(KK,mu,nu,cc,kk):
+    return KK[mu][nu][cc[:kk].sum():cc[:kk].sum()+cc[kk]]
+
+def cc_vals(rr_mu_nu):
+    return ( np.array([c_0,c_1,c_2,rr_mu_nu-c_0-c_1-c_2])
+             for c_0 in range(rr_mu_nu+1)
+             for c_1 in range(rr_mu_nu-c_0+1)
+             for c_2 in range(rr_mu_nu-c_0-c_1+1) )
+
+def QQ(KK,rr):
+    KK_mat = partition(KK,rr)
+
+    QQ = II
     for mu, nu in itertools.product(range(3), repeat = 2):
-        for jj_mu_nu in jj_vals[mu][nu]:
-            op_list[jj_mu_nu] = spin_op(mu) * spin_op(nu)
-            # op_list[jj_mu_nu] = sum([ eta(mu,nu,rho) * spin_op(rho) for rho in range(4)])
-    return qt.tensor(op_list)
+        QQ_mu_nu = 0 * II
+        for cc in cc_vals(rr[mu,nu]):
+            cc_ops = [ I2 ] * N
+            g_fac = g(cc,rr[mu,nu])
+            eta_fac = 1
+            for kk in range(4):
+                eta_fac *= eta(mu,nu,kk)**cc[kk]
+                for idx in KK_cc(KK_mat,mu,nu,cc,kk):
+                    cc_ops[idx] = spin_op(kk)
+            QQ_mu_nu += g_fac * eta_fac * qt.tensor(cc_ops)
+        QQ *= QQ_mu_nu
+
+    return QQ
+
+    # op_list = [ I2 ] * N
+    # for mu, nu in itertools.product(range(3), repeat = 2):
+    #     for KK_mu_nu in KK_mat[mu][nu]:
+    #         op_list[KK_mu_nu] = spin_op(mu) * spin_op(nu)
+
+    # QQ = qt.tensor(op_list)
+    # return QQ
 
 def SS(mm):
     op = 0 * II
     for jj in itertools.permutations(range(N), int(np.sum(mm))):
         op += PP(jj,mm)
     return op
-
-def poch(nn, kk, vals = {}):
-    return np.prod([ nn - cc for cc in range(kk) ])
-
-def f(mm,nn,rr):
-    mnb_fac = np.prod([ poch(mm[mu],rr[mu,:].sum()) * poch(nn[mu],rr[:,mu].sum())
-                       for mu in range(3) ])
-    rr_fac = np.prod([ factorial(rr[mu,nu]) for mu in range(3) for nu in range(3) ])
-    return mnb_fac / rr_fac
 
 def r_mats(mm,nn,ss):
     return ( np.array([ [0,    r_01, r_02],
@@ -116,9 +146,19 @@ for mm in itertools.product(range(op_cap+1), repeat = 3):
 
                 for JK in itertools.permutations(range(N), ops):
                     mn = mm + nn - rr.sum(0) - rr.sum(1)
-                    JJ = JK[:mn.sum()]
-                    KK = JK[mn.sum():]
-                    op_test += f_val * PP(JJ,mn) * QQ(KK,rr)
+
+                    if mn.sum() > 0:
+                        JJ = JK[:mn.sum()]
+                        PP_JJ = PP(JJ,mn)
+                    else:
+                        PP_JJ = II
+                    if mn.sum() < len(JK):
+                        KK = JK[mn.sum():]
+                        QQ_KK = QQ(KK,rr)
+                    else:
+                        QQ_KK = II
+
+                    op_test += f_val * PP_JJ * QQ_KK
 
         print(mm, nn, op == op_test)
         if op != op_test: exit()
