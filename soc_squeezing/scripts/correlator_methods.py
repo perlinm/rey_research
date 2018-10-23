fixed minor bug with printing incorrect time in plot_squeezing split off method to compute derivatives of squeezing operators in correlator_methods added script to compute and save TAT simulation results using both Heisenberg evolution and jump methods

soc_squeezing/scripts/correlator_methods.py
@@ -524,8 +524,10 @@ squeezing_ops = [ (0,1,0), (0,2,0), (1,0,0), (2,0,0), (1,1,0), (1,0,1) ]
 
 # return correlators from evolution under a general Hamiltonian
 def compute_correlators(spin_num, order_cap, chi_times, initial_state, h_vec,
-                        dec_rates = [], dec_mat = None, method = "taylor", mu = 1):
+                        dec_rates = [], dec_mat = None, method = "taylor", mu = 1,
+                        return_derivs = False):
     assert(method == "taylor" or method == "diffeq")
+    if return_derivs: assert(method == "taylor")
 
     state_sign, state_dir = initial_state
     state_dir = initial_state[1]
@@ -555,10 +557,14 @@ def compute_correlators(spin_num, order_cap, chi_times, initial_state, h_vec,
     op_image_args = ( convert_zxy(h_vec,mu), total_spin, dec_vecs, mu )
 
     if method == "taylor":
-        correlators = compute_correlators_taylor(order_cap, chi_times, op_image_args,
-                                                 init_ln_val, init_val_sign, initial_state)
+        derivs = compute_squeezing_derivs(order_cap, op_image_args,
+                                          init_ln_val, init_val_sign, initial_state)
+        if return_derivs: return derivs
+        times_k = np.array([ chi_times**order for order in range(order_cap) ])
+        correlators = { op : derivs[op] @ times_k for op in squeezing_ops }
+
     else: # method == "diffeq"
-        correlators = compute_correlators_diffeq(order_cap, chi_times, op_image_args,
+        correlators = compute_correlators_diffeq(chi_times, order_cap, op_image_args,
                                                  init_ln_val, init_val_sign)
 
     if mu == 1:
@@ -574,9 +580,10 @@ def compute_correlators(spin_num, order_cap, chi_times, initial_state, h_vec,
         return reversed_corrs
 
 
-# compute correlators using a Taylor expansion about time t = 0
-def compute_correlators_taylor(order_cap, chi_times, op_image_args,
-                               init_ln_val, init_val_sign, initial_state):
+# compute derivatives of squeezing operators:
+#   derivs[op][kk] = < (d/dt)^kk op >_0 / kk!
+def compute_squeezing_derivs(order_cap, op_image_args, init_ln_val, init_val_sign,
+                             initial_state):
 
     if initial_state == "-Z":
         chop_operators = True
@@ -635,23 +642,20 @@ def compute_correlators_taylor(order_cap, chi_times, op_image_args,
                         try: init_ln_vals[op[::-1]] = init_ln_vals[op]
                         except: init_ln_vals[op[::-1]] = None
 
-    times_k = np.array([ chi_times**order for order in range(order_cap) ]).astype(complex)
-    correlators = {}
+    derivs = {}
     for sqz_op in squeezing_ops:
-        # compute Q[kk] = < (d/dt)^kk S_\mu^pp (\mu S_\z)^qq S_\nu^rr >_0 / kk! for all kk
-        Q = np.zeros(order_cap, dtype = complex)
+        derivs[sqz_op] = np.zeros(order_cap, dtype = complex)
         for order in range(order_cap):
             for T_op, T_val in time_derivs[sqz_op,order].items():
                 init_ln_val = init_ln_vals[T_op]
                 if init_ln_val is None: continue
                 term_ln_mag = np.log(complex(T_val)) + init_ln_val
-                Q[order] += np.exp(term_ln_mag) * init_val_sign(T_op)
-        correlators[sqz_op] = Q @ times_k
+                derivs[sqz_op][order] += np.exp(term_ln_mag) * init_val_sign(T_op)
 
-    return correlators
+    return derivs
 
 # compute correlators by solving an initial value problem (i.e. differential equation)
-def compute_correlators_diffeq(order_cap, chi_times, op_image_args,
+def compute_correlators_diffeq(chi_times, order_cap, op_image_args,
                                init_ln_val, init_val_sign, ivp_tolerance = 1e-10):
     op_num = 0 # counts number of operaters we keep track of
     op_idx = {} # dictionary taking operator to unique integer index

soc_squeezing/scripts/plot_squeezing.py
@@ -43,7 +43,7 @@ lattice_depth_bounds = (1,15) # min / max lattice depths we will allow
 confining_depth = 60 # lattice depth along confining axis
 
 time_steps = 200 # time steps in plot
-ivp_tolerance = 1e-5 # relative error tolerance in numerical integrator
+ivp_tolerance = 1e-10 # relative error tolerance in numerical integrator
 max_tau = 2 # for simulation: chi * max_time = max_tau * N **(-2/3)
 
 h_U_target = 0.05 # target value of h_std / U_int
@@ -134,7 +134,7 @@ dt = times[-1] / time_steps
 # compute OAT squeezing parameters
 sqz_OAT = squeezing_OAT(N, chi_times)
 print()
-print("t_opt_OAT (sec):", times[sqz_OAT.argmax()])
+print("t_opt_OAT (sec):", times_SI[sqz_OAT.argmax()])
 print("sqz_opt_OAT (dB):", sqz_OAT.max())
 
 # compute Hamiltonian, spin vector, spin-spin matrix, and initial states for TVF and TAT,
@@ -173,28 +173,30 @@ print("t_opt_OAT_D (sec):", times_SI[sqz_OAT_D.argmax()])
 print("sqz_opt_OAT_D (dB):", sqz_OAT_D.max())
 
 # construct TAT Hamiltonian in (z,x,y) format
-h_TAT = { (0,0,2) : +1/3,       # S_y^2 - S_x^2
-          (0,2,0) : -1/3 }
+h_TAT = { (2,0,0) : +1/3,
+          (0,0,2) : -1/3 }
+init_state = "+X"
+init_state_vec = coherent_spin_state([0,1,0], N)
 
-# compute correlators and squeezing for benchmarking
-correlators_TAT_B = compute_correlators(N, order_cap, chi_times, "-Z", h_TAT)
+# construct spin transformation matrix for the TAT protocol
+dec_mat_TAT = dec_mat_drive(scipy.special.jv(0,drive_mod_index_zy))
+
+# compute correlators and squeezing without dechoerence for benchmarking
+correlators_TAT_B = compute_correlators(N, order_cap, chi_times, init_state, h_TAT)
 sqz_TAT_B = squeezing_from_correlators(N, correlators_TAT_B)
 
 del correlators_TAT_B
 
-# construct spin transformation matrix for the TAT protocol
-dec_mat_TAT = dec_mat_drive(scipy.special.jv(0,drive_mod_index_yx_1))
-
 # compute correlators and squeezing with decoherence
-correlators_TAT_D = compute_correlators(N, order_cap, chi_times, "-Z", h_TAT, dec_rates,
-                                        dec_mat_TAT)
+correlators_TAT_D = compute_correlators(N, order_cap, chi_times, init_state, h_TAT,
+                                        dec_rates, dec_mat_TAT)
 sqz_TAT_D = squeezing_from_correlators(N, correlators_TAT_D)
 
 del correlators_TAT_D
 
-initial_state = coherent_spin_state([-1,0,0], N)
-correlators_TAT_J = correlators_from_trajectories(N, chi_times, initial_state, h_TAT,
-                                                  trajectories, dec_rates, dec_mat_TAT)
+# compute correlators and squeezing using the quantum jump method
+jump_args = [ N, trajectories, chi_times, init_state_vec, h_TAT, dec_rates, dec_mat_TAT ]
+correlators_TAT_J = correlators_from_trajectories(*jump_args)
 sqz_TAT_J = squeezing_from_correlators(N, correlators_TAT_J)
 
 del correlators_TAT_J