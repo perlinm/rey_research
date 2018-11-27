343219ab4ccbcbf11dd7eca03c0c37420a5d35af
Michael A. Perlin <mika.perlin@gmail.com>
2018-11-27 14:34:51 -0500

    removed Fermi-Hubbard (FH) squeezing plots

--- soc_squeezing/scripts/plot_squeezing.py
+++ soc_squeezing/scripts/plot_squeezing.py
@@ -10,8 +10,7 @@ from scipy.integrate import solve_ivp
 from mathieu_methods import mathieu_solution
 from overlap_methods import pair_overlap_1D, tunneling_1D
 from dicke_methods import spin_op_vec_mat_dicke, coherent_spin_state
-from fermi_hubbard_methods import sum, prod, get_simulation_parameters, spatial_basis, \
-    get_c_op_mats, spin_op_vec_mat_FH, polarized_states_FH, gauged_energy, hamiltonians
+from fermi_hubbard_methods import get_simulation_parameters, gauged_energy, spatial_basis
 from squeezing_methods import spin_squeezing, squeezing_from_correlators, squeezing_OAT
 from correlator_methods import compute_correlators, dec_mat_drive, convert_zxy
 from jump_methods import correlators_from_trajectories
@@ -36,7 +35,7 @@ plt.rcParams.update(params)
 L = [10]*2 # lattice sites
 order_cap = 40 # order limit for cumulant expansions
 U_J_target = 2 # target value of U_int / J_0
-trajectories = 100 # number of trajectories to use in quantum jump simulations
+trajectories = 500 # number of trajectories to use in quantum jump simulations
 
 site_number = 1000 # number of sites in lattice calculations
 lattice_depth_bounds = (1,15) # min / max lattice depths we will allow
@@ -47,7 +46,6 @@ ivp_tolerance = 1e-10 # relative error tolerance in numerical integrator
 max_tau = 2 # for simulation: chi * max_time = max_tau * N **(-2/3)
 
 h_U_target = 0.05 # target value of h_std / U_int
-fermi_N_max = 8 # maximum number of atoms for which to run Fermi Hubbard calculations
 
 excited_lifetime_SI = 10 # seconds; lifetime of excited state (from e --> g decay)
 drive_mod_index_zy = 0.9057195866712102 # for TAT protocol about (z,y)
@@ -59,8 +57,8 @@ drive_mod_index_yx_2 = 2.2213461342426544 # for TAT protocol about (y,x)
 # compute all experimental parameters
 ##########################################################################################
 
-N = prod(L)
-eta = N / prod(L)
+N = np.product(L)
+eta = N / np.product(L)
 lattice_dim = np.array(L, ndmin = 1).size
 
 # determine primary lattice depth which optimally satisfies our target U_int / J_0
@@ -79,7 +77,7 @@ lattice_depth = minimize_scalar(lambda x: abs(U_J(x)-U_J_target),
 # get simulation parameters and on-site interaction energy
 L, J_0, J_T, K_0, K_T, momenta, fourier_vecs, energies = \
     get_simulation_parameters(L, lattice_depth, confining_depth, site_number)
-U_int = g_int_LU[1] * K_T**(3-lattice_dim) * prod(K_0)
+U_int = g_int_LU[1] * K_T**(3-lattice_dim) * np.product(K_0)
 
 # determine optimal SOC angle using hubbard approximation
 def h_std(phi): return 2**(1+lattice_dim/2)*J_0[0]*np.sin(phi/2)
@@ -203,54 +201,6 @@ del correlators_TAT_J
 
 
 ##########################################################################################
-# compute squeezing parameters with exact simulations
-##########################################################################################
-
-# if we have few enough particles, compute FH squeezing
-if N <= fermi_N_max:
-    hilbert_dim = int(scipy.special.binom(2*prod(L),N))
-    print("Fermi-Hubbard hilbert space dimension:", hilbert_dim)
-
-    c_op_mats = get_c_op_mats(L, N, depth = 2)
-    S_op_vec, SS_op_mat = spin_op_vec_mat_FH(L, N, c_op_mats)
-    H_lat, H_int, H_clock = hamiltonians(N, L, phi, lattice_depth,
-                                         confining_depth, c_op_mats)
-    H_SS = H_lat - U_int / prod(L) * sum([ SS_op_mat[ii][ii] for ii in range(3) ])
-    H_free = H_lat + H_int
-
-    def H_periodic(t):
-        return H_free + drive_mod_index_yx_1 * omega * np.cos(omega * t) * H_clock
-
-    def deriv_SS(time, state): return -1j * H_SS.dot(state)
-    def deriv_free(time, state): return -1j * H_free.dot(state)
-    def deriv_periodic(time, state): return -1j * H_periodic(time).dot(state)
-
-    state_z, state_x, state_y = [ state.toarray().flatten().astype(complex)
-                                  for state in polarized_states_FH(L, N) ]
-
-    init_SS = state_y.copy()
-    init_free = state_y.copy()
-    init_periodic = state_z.copy()
-
-    state_SS = solve_ivp(deriv_SS, (0,times[-1]), init_SS,
-                         t_eval = times, max_step = times[1]).y
-    state_free = solve_ivp(deriv_free, (0,times[-1]), init_free,
-                           t_eval = times, max_step = times[1]).y
-    state_periodic = solve_ivp(deriv_periodic, (0,times[-1]), init_periodic,
-                               t_eval = times, max_step = times[1]).y
-
-    S_ops = (S_op_vec, SS_op_mat)
-    sqz_SS = np.array([ spin_squeezing(N, state_SS[:,tt], *S_ops)
-                        for tt in range(times.size) ])
-    sqz_free = np.array([ spin_squeezing(N, state_free[:,tt], *S_ops)
-                          for tt in range(times.size) ])
-    sqz_periodic = np.array([ spin_squeezing(N, state_periodic[:,tt], *S_ops)
-                              for tt in range(times.size) ])
-
-    del state_SS, state_free, state_periodic
-
-
-##########################################################################################
 # make squeezing plot
 ##########################################################################################
 
@@ -277,12 +227,6 @@ lim_TAT_D = plot_lim_idx(sqz_TAT_D)
 plt.plot(times_SI[:lim_TAT_D], sqz_TAT_D[:lim_TAT_D],
          ":", color = line_TAT.get_color())
 plt.plot(times_SI, sqz_TAT_J, ".", color = line_TAT.get_color())
-
-try:
-    plt.plot(times_SI, sqz_SS, label = "SS")
-    plt.plot(times_SI, sqz_free, label = "FH (free)")
-    plt.plot(times_SI, sqz_periodic, label = "FH (periodic)")
-except: None
 
 plt.xlim(0,times_SI[-1])
 plt.ylim(0,sqz_TAT.max()*1.1)
