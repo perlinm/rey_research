9cd5d01e5ae227ff8429c65f153ceadce79401c4
Michael A. Perlin <mika.perlin@gmail.com>
2018-12-05 10:41:40 -0700

    fixed up legend in benchmarking figure

--- soc_squeezing/scripts/plot_benchmarking.m
+++ soc_squeezing/scripts/plot_benchmarking.m
@@ -1,7 +1,11 @@
 clear all;
 
-% set figure size and directories
+% set figure options
 fig_size = [ 6.5 5 ];
+color_FH = [ .1 .6 .85 ];
+color_OAT = [ .5 .5 .5 ];
+
+% set directories
 fig_dir = "../figures/";
 data_dir = "../data/model_benchmarking/";
 
@@ -15,7 +19,8 @@ phi_vals = transpose(phi_vals);
 
 % shorthand for the function which makes surface plots
 make_plot = @(N_tag, dtype, title_text) ...
-    make_plot_full(N_tag, dtype, title_text, data_dir, U_vals, phi_vals);
+    make_plot_full(N_tag, dtype, title_text, ...
+                   data_dir, U_vals, phi_vals, color_FH, color_OAT);
 
 % make all surface plots
 figure;
@@ -30,15 +35,27 @@ subplot(2,2,4); % bottom right
 make_plot('09', 't', '(b.ii)');
 
 % make legend
-subplot(2,2,1); % top left
-leg = legend('FH', 'Spin', 'OAT');
-leg.Location = 'east';
-leg.Units = 'normalized';
-width = leg.Position(3);
-height = leg.Position(4);
-left = 0.5 - width/2;
-bottom = 0.5 - height/2;
-leg.Position = [ left bottom width height ];
+% subplot(2,2,1); % top left
+% patches(1) = patch(NaN, NaN, color_FH, 'EdgeAlpha', 0);
+% %patches(2) = plot(NaN, NaN, 'k', 'LineWidth', .8);
+% patches(2) = patch(NaN, NaN, 'w');
+% patches(3) = patch(NaN, NaN, color_OAT, 'EdgeAlpha', 0, 'FaceAlpha', 0.5);
+% leg = legend(patches, 'FH', 'Spin', 'OAT');
+% leg.Location = 'east';
+% leg.Units = 'normalized';
+% width = leg.Position(3);
+% height = leg.Position(4);
+% left = 0.5 - width/2;
+% bottom = 0.5 - height/2;
+% leg.Position = [ left bottom width height ];
+bars = bar(rand(2,3),'Visible','off');
+set(bars(1), 'FaceColor', color_FH, 'EdgeAlpha', 0);
+set(bars(2), 'FaceColor', 'w');
+set(bars(3), 'FaceColor', color_OAT, 'EdgeAlpha', 0, 'FaceAlpha', 0.5);
+[legend_obj,patch_obj,~,~] = ...
+    legendflex(bars, {'FH', 'Spin', 'OAT'}, ...
+                      'ref', gcf, 'anchor', {'s' 's'}, 'bufferunit', 'normalized', 'buffer', [0 0.45]);
+hatchfill2(patch_obj(length(bars)+2), 'cross', 'HatchAngle', 0, 'HatchDensity', 6);
 
 % scale figure properly and save it to a file
 set(gcf, 'Units', 'inches');
@@ -46,12 +63,8 @@ set(gcf, 'OuterPosition', [ 0 0 fig_size(1) fig_size(2) ]);
 print(fig_dir + 'model_benchmarking.png', '-dpng', '-r600');
 
 % function to make one surface plot comparing FH, spin, and OAT models
-function make_plot_full(N_tag, dtype, title_text, data_dir, U_vals, phi_vals)
-    % set surface colors for each model
-    color_FH = [ .1 .6 .85 ];
-    color_spin = [ 1 1 1 ];
-    color_OAT = [ .5 .5 .5 ];
-
+function make_plot_full(N_tag, dtype, title_text, ...
+                        data_dir, U_vals, phi_vals, color_FH, color_OAT)
     % scale data by 2\pi if we are plotting squeezing times
     if dtype == 't'
         data_scale = 2*pi;
@@ -66,11 +79,12 @@ function make_plot_full(N_tag, dtype, title_text, data_dir, U_vals, phi_vals)
     data_OAT(1,:) = data_OAT(2,:); % correct for an artifact of convention at U/J = 0
 
     % make all surface plots
-    surf(U_vals, phi_vals, data_FH, 'FaceColor', color_FH, 'FaceAlpha', 1, 'EdgeAlpha', 0); hold on;
-    surf(U_vals, phi_vals, data_spin, 'FaceColor', color_spin, 'FaceAlpha', 0, 'EdgeAlpha', 1);
-    surf(U_vals, phi_vals, data_OAT, 'FaceColor', color_OAT, 'FaceAlpha', 0.5, 'EdgeAlpha', 0);
+    surf(U_vals, phi_vals, data_FH, 'FaceColor', color_FH, 'EdgeAlpha', 0); hold on;
+    mesh(U_vals, phi_vals, data_spin, 'FaceAlpha', 0, 'EdgeColor', [0 0 0]);
+    surf(U_vals, phi_vals, data_OAT, 'FaceColor', color_OAT, 'EdgeAlpha', 0, 'FaceAlpha', 0.5);
 
     % set axis ticks and labels
+    set(gca, 'XLim', [0 8]);
     set(gca, 'XTick', 0:2:8);
     set(gca, 'YLim', [-2 -1]);
     set(gca, 'YTick', -2:0.5:-1);
