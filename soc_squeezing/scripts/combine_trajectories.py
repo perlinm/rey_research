c30122b1183b7d6a484bea56acc19281106b25ea
Michael A. Perlin <mika.perlin@gmail.com>
2018-12-10 22:04:19 -0700

    added scripts to run trajectory simulations on terra, and combine results from multiple runs

---
+++ soc_squeezing/scripts/combine_trajectories.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+
+import os, glob, re
+import numpy as np
+
+from squeezing_methods import squeezing_from_correlators
+
+log10_N = 4
+method = "TAT"
+time_steps = 200
+
+files = glob.glob(f"../data/squeezing/*/*logN{log10_N}*{method}*.txt")
+data = sum([ np.loadtxt(f, dtype = complex, delimiter = ",")
+             for f in files ]) / len(files)
+
+sqz_name = os.path.basename(re.sub(r"_s[0-9]{3}", "", files[0]))
+sqz_path = os.path.dirname(files[0]) + "/../" + sqz_name
+
+header = ""
+header += r"# first column: time in units of the OAT stregth \chi" + "\n"
+header += r"# second column: squeezing as \xi^2" + "\n"
+
+with open(files[0], "r") as f:
+    for line in f:
+        if "trajectories" in line:
+            trajectories_per_run = int(line.split()[-1])
+        elif "max_tau" in line:
+            max_tau = int(line.split()[-1])
+        elif "ops" in line:
+            nums = np.array([ int(nn) for nn in re.findall(r"[0-9]+", line) ])
+            nums = nums.reshape((nums.size//3,3))
+            ops = [ tuple(nums[jj,:]) for jj in range(nums.shape[0]) ]
+        elif line[0] != "#": break
+
+header += f"# trajectories: {len(files)*trajectories_per_run}\n"
+
+N = 10**log10_N
+max_time = max_tau * N**(-2/3)
+times = np.linspace(0, max_time, time_steps)
+
+correlators = { ops[jj] : data[jj,:] for jj in range(len(ops)) }
+sqz = squeezing_from_correlators(N, correlators)
+
+with open(sqz_path, "w") as f:
+    f.write(header)
+    for tt in range(times.size):
+        f.write(f"{times[tt]},{sqz[tt]}\n")
---
+++ soc_squeezing/scripts/run_trajectories.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python3
+
+import sys
+import numpy as np
+
+from dicke_methods import coherent_spin_state
+from correlator_methods import compute_correlators, convert_zxy_mat, convert_zxy
+from jump_methods import correlators_from_trajectories
+
+assert(len(sys.argv[1:]) in [ 2, 3 ])
+method = sys.argv[1]
+seed = int(sys.argv[2])
+
+TAT, TNT = "TAT", "TNT"
+if method == TAT:
+    memory = "5G"
+elif method == TNT:
+    memory = "10G"
+else:
+    print("method must be one of TAT or TNT")
+    exit()
+
+log10_N = 4
+N = 10**log10_N
+trajectories = 100
+
+log_dir = "./logs/"
+data_dir = "../data/squeezing/jumps/"
+job_name = f"sqz_D_exact_logN{log10_N}_{method}_s{seed:03d}"
+
+log_text = f"""#!/bin/sh
+
+#SBATCH --partition=nistq,jila
+#SBATCH --mem={memory}
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --job-name={job_name}
+#SBATCH --output={log_dir}{job_name}.o
+#SBATCH --error={log_dir}{job_name}.e
+#SBATCH --time=05-00
+
+module load python3
+
+python3 {sys.argv[0]} {method} {seed}
+"""
+
+if len(sys.argv[1:]) == 3:
+    with open(log_dir + job_name + ".sh", "w") as f:
+        f.write(log_text)
+    exit()
+
+max_tau = 2
+time_steps = 200
+dec_rates = [ (1,1,1), (0,0,0) ]
+
+init_state = "-Z"
+h_TAT = { (0,2,0) : +1/3,
+          (0,0,2) : -1/3 }
+h_TNT = { (0,2,0) : 1,
+          (1,0,0) : -N/2 }
+h_vec = { TAT : h_TAT, TNT : h_TNT }
+solver = { TAT : "RK45", TNT : "RK45" }
+
+dec_mat_zxy = np.array([ [ 0, -1, 0 ],
+                         [ 1,  0, 0 ],
+                         [ 0,  0, 1 ]])
+dec_mat = convert_zxy_mat(dec_mat_zxy)
+
+max_time = max_tau * N**(-2/3)
+times = np.linspace(0, max_time, time_steps)
+
+init_state_vec = coherent_spin_state(init_state, N)
+def jump_args(hamiltonian):
+    return [ N, trajectories, times, init_state_vec, hamiltonian, dec_rates, dec_mat ]
+
+correlators = correlators_from_trajectories(*jump_args(h_vec[method]),
+                                            seed = seed, solver = solver[method])
+with open(data_dir + job_name + ".txt", "w") as f:
+    f.write(f"# trajectories: {trajectories}\n")
+    f.write(f"# max_tau: {max_tau}\n")
+    ops = list(correlators.keys())
+    f.write("# ops: " + " ".join([ str(op) for op in ops]) + "\n")
+    for op in ops:
+        f.write(",".join([ str(val) for val in correlators[op] ]) + "\n")
+
