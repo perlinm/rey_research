b74dc18b5cc28bc030e61232742045ff7c9c8918
Michael A. Perlin <mika.perlin@gmail.com>
2018-12-16 20:32:20 -0700

    changed naming convention for systems / subsystems exported entropy vector methods to their own file

---
+++ entropy/entropy_methods.py
@@ -0,0 +1,246 @@
+#!/usr/bin/env python2
+
+import itertools
+
+# split a system into its primary subsystems
+def primary_subsystems(system):
+    systems = []
+    start_idx = 0
+    while start_idx < len(system):
+        if system[start_idx] == " ":
+            start_idx += 1
+            continue
+        end_idx = start_idx + 1
+        while end_idx < len(system) and not system[end_idx].isalpha():
+            end_idx += 1
+        systems.append(system[start_idx:end_idx])
+        start_idx = end_idx
+    return systems
+
+# return power set of a collection of objects
+def power_set(collection):
+    if type(collection) == str:
+        collection = primary_subsystems(collection)
+    iterable = itertools.chain.from_iterable(itertools.combinations(collection, nn)
+                                             for nn in range(len(collection)+1))
+    return list( list(part) for part in iterable )
+
+# entropy vector object class
+class e_vec:
+    # initialize an entropy vector
+    def __init__(self, *args):
+        if len(args) == 1: # args is a list of pairs: (subsystem, coefficient)
+            self.subs, self.vals = zip(*args[0])
+        elif len(args) == 2: # args is two ordered lists: subsystems, coefficients
+            self.subs, self.vals = args[0], args[1]
+        elif len(args) == 3:
+            # args[0] is an ordered list of primary systems
+            # args[1] is an ordered list of subsystems
+            # args[2] is an ordered list of coefficients
+            self.systems, self.subs, self.vals = args
+        elif len(args) == 0: # empty vector
+            self.systems, self.subs, self.vals = [], [], []
+            return
+        else:
+            print("invalid initialization of an entropy vector!")
+            exit()
+
+        if len(self.subs) == 0:
+            self.systems = []
+            return
+
+        # if necessary, properly identify primary systems and subsystems
+        if len(args) in [ 1, 2 ]:
+            self.subs = list(self.subs)
+            self.systems = sorted(list(set.union(*[ set(sub) for sub in self.subs ])))
+
+        self.sort()
+        self.simplify()
+
+    # sort vector subsystems and coefficients
+    def sort(self):
+        # sort the systems within each subsystem
+        self.subs = [ sorted(sub, key = lambda sys : self.systems.index(sys))
+                       for sub in self.subs ]
+        # sort the list of subsystems and corresponding values
+        def subsystem_sort_key(sub):
+            return tuple([len(sub)] + [self.systems.index(sys) for sys in sub])
+        zipped_vec = sorted(zip(self.subs, self.vals),
+                            key = lambda x : subsystem_sort_key(x[0]))
+        self.subs, self.vals = map(list,zip(*zipped_vec))
+
+    # simplify entropy vector
+    def simplify(self):
+        # combine identical subsystems
+        text_subs = [ "".join(sub) for sub in self.subs ]
+        for sub in set(text_subs):
+            indices = [ jj for jj in range(len(text_subs)) if text_subs[jj] == sub ]
+            if len(indices) > 1:
+                self.vals[indices[0]] = sum([ self.vals[jj] for jj in indices ])
+                for jj in indices[1:][::-1]:
+                    del self.vals[jj]
+                    del self.subs[jj]
+                    del text_subs[jj]
+
+        # remove zeros
+        zero_indices = [ jj for jj in range(len(self.vals)) if self.vals[jj] == 0 ]
+        for idx in zero_indices[::-1]:
+            del self.subs[idx]
+            del self.vals[idx]
+
+    # return new entropy vector with relabeled systems
+    # note: always splits resulting vector
+    def relabeled(self, *args):
+        args = list(args)
+        if len(args) == 1:
+            # args is a single list of new subsystem names
+            old_systems, new_systems = self.systems, args[0]
+        elif len(args) == 2 and type(args[0]) == list:
+            # args is two lists: old subsystems and new ones
+            old_systems, new_systems = args
+        else:
+            # args is two strings: an old subsystem and its new name
+            old_systems, new_systems = [args[0]], [args[1]]
+
+        def relabel(system):
+            if system in old_systems:
+                return new_systems[old_systems.index(system)]
+            else: return system
+
+        systems = [ relabel(sys) for sys in self.systems ]
+        subsystems = [ [ relabel(sys) for sys in sub ]
+                       for sub in self.subs ]
+        return e_vec(systems, subsystems, self.vals).split()
+
+    # return new entropy vector with split subsystems
+    def split(self):
+        return e_vec([ "".join(sub) for sub in self.subs ], self.vals)
+
+    # returns a full vector in an ordered basis
+    def standard_form(self, system = None):
+        if system == None: subs = self.systems
+        else: subs = system
+        sub_power_set = list(power_set(subs))[1:]
+        if any(part not in sub_power_set for part in self.subs):
+            return self.split().standard_form(system)
+        vec = [ 0 ] * len(sub_power_set)
+        for part, val in zip(self.subs, self.vals):
+            vec[sub_power_set.index(part)] = val
+        return vec
+
+    # return "purification dual" with respect to a given primary system
+    def purification_dual(self, system):
+        subs_set = set(range(len(self.systems)))
+        idx = self.systems.index(system)
+        def trim(part): return set(part).difference(set([idx]))
+        def dual_part(part): return sorted(list(subs_set.difference(trim(part))))
+        new_parts = [ part if idx not in part else dual_part(part)
+                      for part in self.subs ]
+        new_vec = e_vec(self.systems, new_parts, self.vals)
+        return new_vec
+
+    # evaluate symbolic entries according to dictionary
+    def evaluated(self, dict):
+        vals = [ val if isinstance(val, (int)) else int(val.subs(dict))
+                 for val in self.vals ]
+        return e_vec(self.systems, self.subs, vals)
+
+    # reduced "reduced" vector obtained by making some subsystem trivial
+    def reduce(self, trivial_subsystem):
+        if type(trivial_subsystem) == str:
+            trivial_subsystem = primary_subsystems(trivial_subsystem)
+        systems = [ sys for sys in self.systems if sys not in trivial_subsystem ]
+        subsystems = [ [ sys for sys in part if sys not in trivial_subsystem ]
+                       for part in self.subs ]
+        if all( len(sub) == 0 for sub in subsystems ): return e_vec()
+        subsystems, vals = zip(*[ (sub, val) for sub, val in zip(subsystems, self.vals)
+                                  if len(sub) > 0 ])
+        return e_vec(systems, subsystems, vals)
+
+    # return human-readable text identifying this entropy vector
+    def __repr__(self):
+        signs = [ "+" if val > 0 else "-" for val in self.vals ]
+        abs_vals = [ "" if val in [1,-1]
+                     else ( " {}".format(val) if val > 0 else " {}".format(-val) )
+                     for val in self.vals]
+        text_subs = [ "".join(sub) for sub in self.subs ]
+        return " ".join([ "{}{} ({})".format(sign,abs_val,sub)
+                          for sign,abs_val,sub in zip(signs,abs_vals,text_subs) ])
+
+    def __hash__(self):
+        return hash(self.split().__repr__())
+
+    # relations and operations
+    def __eq__(self, other): # comparison x == y
+        return self.__hash__() == other.__hash__()
+    def __ne__(self, other): # comparison x != y
+        return not (self == other)
+    def __pos__(self): return self # unary "+"
+    def __neg__(self): # unary "-", i.e. negation
+        return e_vec(self.systems, self.subs, [ -val for val in self.vals ])
+    def __add__(self, other): # addition of vectors: x + y
+        left, right = self.split(), other.split()
+        systems = sorted(list(set(left.systems + right.systems)))
+        subs = left.subs + right.subs
+        vals = left.vals + right.vals
+        new_vec = e_vec(systems, subs, vals)
+        new_vec.sort()
+        new_vec.simplify()
+        return new_vec
+    def __sub__(self, other): return self + (-other) # subtraction of vectors: x - y
+
+# return iterator over all partitions of a collection of items
+def get_all_partitions(collection):
+    if len(collection) == 1:
+        yield [ collection ]
+        return
+    first = collection[0]
+    for smaller in get_all_partitions(collection[1:]):
+        for n, subset in enumerate(smaller):
+            yield smaller[:n] + [[ first ] + subset]  + smaller[n+1:]
+        yield [ [ first ] ] + smaller
+
+# get all vectors acquired by subsystem permutation, purification duals
+def get_seeded_vecs(seed_vec):
+    # get all unique permutations of the input vector
+    permutation_vecs = set( seed_vec.relabeled(permutation)
+                            for permutation in itertools.permutations(seed_vec.systems) )
+
+    # get all purification duals of all permutation vectors
+    purification_vecs = set( vec.purification_dual(sys)
+                             for vec in permutation_vecs
+                             for sys in vec.systems )
+
+    # collect all vectors obtained by additionally making subsystems trivial
+    seeded_vecs = set( vec.reduce(subsystem)
+                       for vec in set.union(permutation_vecs, purification_vecs)
+                       for subsystem in power_set(vec.systems) )
+    return set([ vec for vec in seeded_vecs if vec != e_vec() ])
+
+# given a seed vector, compute all corresponding "mirror vectors",
+# then collect all vectors of the same form as the mirror vectors for a larger system
+def get_super_vectors(systems, seed_vector):
+    if type(systems) is str:
+        systems = primary_subsystems(systems)
+    seeded_vectors = get_seeded_vecs(seed_vector)
+
+    system_vecs = set() # keep track of vectors addressing the entire system
+
+    # the seeded vectors address k-partite systems for k >= 2; our system is n-partite,
+    # so for each size p from 2 to n
+    for size in range(2, len(systems)+1):
+        # consider all p-partite combinations of the n primary systems
+        for combination in itertools.combinations(systems, size):
+            # for each of these p-partite combinations, consider all k-partitions
+            for partition in get_all_partitions(list(combination)):
+                # generate all mirror vectors on this k-partition
+                new_systems = [ "".join(sorted(parts)) for parts in partition ]
+                system_vecs.update([ vec.relabeled(new_systems)
+                                          for vec in seeded_vectors
+                                          if len(vec.systems) == len(new_systems) ])
+
+    # collect all vectors obtained by making subsystems trivial
+    subsystem_vecs = set( vec.reduce(sys)
+                       for vec in system_vecs
+                       for sys in power_set(vec.systems) )
+    return set([ vec for vec in subsystem_vecs if vec != e_vec() ])
--- entropy/run.py
+++ entropy/run.py
@@ -1,290 +1,76 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python2
 
-import itertools, sympy
+import sys, sympy, sage
 
-def is_numeric(val): return isinstance(val, (int, float))
+from entropy_methods import *
 
-# split partition into subsystems
-def primary_subsystems(partition):
-    subsystems = []
-    start_idx = 0
-    while start_idx < len(partition):
-        if partition[start_idx] == " ":
-            start_idx += 1
-            continue
-        end_idx = start_idx + 1
-        while end_idx < len(partition) and not partition[end_idx].isalpha():
-            end_idx += 1
-        subsystems.append(partition[start_idx:end_idx])
-        start_idx = end_idx
-    return subsystems
+if not len(sys.argv) == 2 or not sys.argv[1].isdigit():
+    print("usage: {} party_number".format(sys.argv[0]))
+    exit()
 
-# return power set of a list of objects
-def power_set(collection):
-    if type(collection) == str:
-        collection = primary_subsystems(collection)
-    iterable = itertools.chain.from_iterable(itertools.combinations(collection, nn)
-                                             for nn in range(len(collection)+1))
-    return list( list(part) for part in iterable )
+system_num = int(sys.argv[1])
+if system_num > 10:
+    print("no.")
+    exit()
 
-# entropy vector object class
-class e_vec:
-    # initialize an entropy vector
-    def __init__(self, *args):
-        if len(args) == 1: # args is a list of pairs: (partition, coefficient)
-            self.parts, self.vals = zip(*args[0])
-        elif len(args) == 2: # args is two ordered lists: partitions, coefficients
-            self.parts, self.vals = args[0], args[1]
-        elif len(args) == 3:
-            # args[0] is an ordered list of subsystems
-            # args[1] is an ordered list of partitions (by index of subsystems)
-            # args[2] is an ordered list of coefficients
-            self.subs, self.parts, self.vals = args
-        elif len(args) == 0: # empty vector
-            self.subs, self.parts, self.vals = [], [], []
-        else:
-            print("invalid initialization of an entropy vector!")
-            exit()
+# first system_num capital letters
+systems = [ chr(jj) for jj in range(ord("A"), ord("Z")) ][:system_num]
+systems_ext = systems + [ "Z" ]
 
-        if len(self.parts) == 0:
-            self.subs = []
-            return
-
-        # if necessary, properly identify subsystems and partitions (by index of subsystem)
-        if len(args) in [ 1, 2 ]:
-            if type(self.parts) is str:
-                self.parts = [ primary_subsystems(part) for part in self.parts ]
-            else:
-                self.parts = list(self.parts)
-            self.subs = sorted(list(set.union(*[ set(part) for part in self.parts ])))
-
-        self.sort()
-        self.simplify()
-
-    # sort vector partitions and coefficients
-    def sort(self):
-        # sort the subsystems within each partition
-        self.parts = [ sorted(part, key = lambda sys : self.subs.index(sys))
-                       for part in self.parts ]
-        # sort the list of partitions and corresponding values
-        def partition_sort_key(part):
-            return tuple([len(part)] + [self.subs.index(sys) for sys in part])
-        zipped_vec = sorted(zip(self.parts, self.vals),
-                            key = lambda x : partition_sort_key(x[0]))
-        self.parts, self.vals = map(list,zip(*zipped_vec))
-
-    # simplify entropy vector
-    def simplify(self):
-        # combine identical partitions
-        text_parts = [ "".join(part) for part in self.parts ]
-        for part in set(text_parts):
-            indices = [ jj for jj in range(len(text_parts)) if text_parts[jj] == part ]
-            if len(indices) > 1:
-                self.vals[indices[0]] = sum([ self.vals[jj] for jj in indices ])
-                for jj in indices[1:][::-1]:
-                    del self.vals[jj]
-                    del self.parts[jj]
-                    del text_parts[jj]
-
-        # remove zeros
-        zero_indices = [ jj for jj in range(len(self.vals)) if self.vals[jj] == 0 ]
-        for idx in zero_indices[::-1]:
-            del self.parts[idx]
-            del self.vals[idx]
-
-    # return new entropy vector with relabeled subsystems
-    def relabeled(self, *args, split = True):
-        args = list(args)
-        if len(args) == 1:
-            # args is a single list of new subsystem names
-            old_subsystems, new_subsystems = self.subs, args[0]
-        elif len(args) == 2 and type(args[0]) == list:
-            # args is two lists: old subsystems and new ones
-            old_subsystems, new_subsystems = args
-        else:
-            # args is two strings: an old subsystem and its new name
-            old_subsystems, new_subsystems = [args[0]], [args[1]]
-
-        def relabel(system):
-            if system in old_subsystems:
-                return new_subsystems[old_subsystems.index(system)]
-            else: return system
-
-        subsystems = [ relabel(sys) for sys in self.subs ]
-        partitions = [ [ relabel(sys) for sys in part ]
-                       for part in self.parts ]
-        new_vec = e_vec(subsystems, partitions, self.vals)
-        if split: return new_vec.split()
-        else: return new_vec
-
-    # return new entropy vector with split subsystems
-    def split(self):
-        return e_vec([ "".join(part) for part in self.parts ], self.vals)
-
-    # returns a full vector in an ordered basis
-    def standard_form(self, system = None):
-        if system == None: subs = self.subs
-        else: subs = system
-        sub_power_set = list(power_set(subs))[1:]
-        if any(part not in sub_power_set for part in self.parts):
-            return self.split().standard_form(system)
-        vec = [ 0 ] * len(sub_power_set)
-        for part, val in zip(self.parts, self.vals):
-            vec[sub_power_set.index(part)] = val
-        return vec
-
-    # return "purification dual" with respect to a given subsystem
-    def purification_dual(self, subsystem):
-        subs_set = set(range(len(self.subs)))
-        idx = self.subs.index(subsystem)
-        def trim(part): return set(part).difference(set([idx]))
-        def dual_part(part): return sorted(list(subs_set.difference(trim(part))))
-        new_parts = [ part if idx not in part else dual_part(part)
-                      for part in self.parts ]
-        new_vec = e_vec(self.subs, new_parts, self.vals)
-        return new_vec
-
-    # evaluate symbolic entries according to dictionary
-    def evaluated(self, dict):
-        vals = [ val if is_numeric(val) else val.subs(dict) for val in self.vals ]
-        return e_vec(self.subs, self.parts, vals)
-
-    # reduced "reduced" vector obtained by making some subsystems trivial
-    def reduce(self, trivial_subsystems):
-        if type(trivial_subsystems) == str:
-            trivial_subsystems = primary_subsystems(trivial_subsystems)
-        subs = [ sys for sys in self.subs if sys not in trivial_subsystems ]
-        parts = [ [ sys for sys in part if sys not in trivial_subsystems ]
-                  for part in self.parts ]
-        if all( len(part) == 0 for part in parts ): return e_vec()
-        parts, vals = zip(*[ (part, val) for part, val in zip(parts, self.vals)
-                             if len(part) > 0])
-        return e_vec(subs, parts, vals)
-
-    # return human-readable text identifying this entropy vector
-    def __repr__(self):
-        signs = [ "+" if val > 0 else "-" for val in self.vals ]
-        abs_vals = [ "" if val in [1,-1]
-                     else ( f" {val}" if val > 0 else f" {-val}" )
-                     for val in self.vals]
-        text_parts = [ "".join(part) for part in self.parts ]
-        return " ".join([ "{}{} ({})".format(sign,abs_val,part)
-                          for sign,abs_val,part in zip(signs,abs_vals,text_parts) ])
-
-    def __hash__(self):
-        return hash(self.split().__repr__())
-
-    # relations and operations
-    def __eq__(self, other): # comparison x == y
-        return self.__hash__() == other.__hash__()
-    def __pos__(self): return self # unary "+"
-    def __neg__(self): # unary "-", i.e. negation
-        return e_vec(self.subs, self.parts, [ -val for val in self.vals ])
-    def __add__(self, other): # addition of vectors: x + y
-        left, right = self.split(), other.split()
-        subs = sorted(list(set(left.subs + right.subs)))
-        parts = left.parts + right.parts
-        vals = left.vals + right.vals
-        new_vec = e_vec(subs, parts, vals)
-        new_vec.sort()
-        new_vec.simplify()
-        return new_vec
-    def __sub__(self, other): return self + (-other) # subtraction of vectors: x - y
-
-# return iterator over all partitions of a collection of items
-def get_all_partitions(collection):
-    if len(collection) == 1:
-        yield [ collection ]
-        return
-    first = collection[0]
-    for smaller in get_all_partitions(collection[1:]):
-        for n, subset in enumerate(smaller):
-            yield smaller[:n] + [[ first ] + subset]  + smaller[n+1:]
-        yield [ [ first ] ] + smaller
-
-# get all vectors acquired by subsystem permutation, purification duals
-def get_seeded_vecs(seed_vec):
-    # get all unique permutations of the input vector
-    permutation_vecs = set( seed_vec.relabeled(permutation).split()
-                            for permutation in itertools.permutations(seed_vec.subs) )
-
-    # get all purification duals of all permutation vectors
-    purification_vecs = set( vec.purification_dual(sub)
-                             for vec in permutation_vecs
-                             for sub in vec.subs )
-
-    # collect all vectors obtained by additionally making subsystems trivial
-    seeded_vecs = set( vec.reduce(subset)
-                       for vec in set.union(permutation_vecs, purification_vecs)
-                       for subset in power_set(vec.subs) )
-    return set([ vec for vec in seeded_vecs if vec != e_vec() ])
-
-# given a seed vector, compute all corresponding "mirror vectors",
-# then collect all vectors of the same form as the mirror vectors for a larger system
-def get_super_vectors(subsystem_text, seed_vector):
-    subsystems = primary_subsystems(subsystem_text)
-    seeded_vectors = get_seeded_vecs(seed_vector)
-
-    supersystem_vecs = set() # keep track of vectors addressing the entire system
-
-    # the seeded vectors address k-partite systems for k >= 2; our system is n-partite,
-    # so for each size p from 2 to n
-    for size in range(2, len(subsystems)+1):
-        # consider all p-partite combinations of the n subsystems
-        for combination in itertools.combinations(subsystems, size):
-            # for each of these p-partite combinations, consider all k-partitions
-            for partition in get_all_partitions(list(combination)):
-                # generate all mirror vectors on this k-partition
-                new_subsystems = [ "".join(sorted(parts)) for parts in partition ]
-                supersystem_vecs.update([ vec.relabeled(new_subsystems)
-                                          for vec in seeded_vectors
-                                          if len(new_subsystems) == len(vec.subs) ])
-
-    # collect all vectors obtained by making subsystems trivial
-    system_vecs = set( vec.reduce(subset)
-                       for vec in supersystem_vecs
-                       for subset in power_set(vec.subs) )
-    return set([ vec for vec in system_vecs if vec != e_vec() ])
-
-# subadditivity, strong subadditivity, and negativity of tripartite information
+# subadditivity, strong subadditivity, and monogamy of mutual information
 SA = e_vec([ ("A",1), ("B",1), ("AB",-1) ])
 SSA = e_vec([ ("AB",1), ("BC",1), ("B",-1), ("ABC",-1) ])
 MMI = e_vec([ ("AB",1), ("AC",1), ("BC",1), ("A",-1), ("B",-1), ("C",-1), ("ABC",-1) ])
 
-systems = "AB"
-partitions = [ "".join(subsystem)
-               for subsystem in list(power_set(systems))[1:] ]
-symbols = [ sympy.symbols(part.lower(), positive = True)
-            for part in partitions ]
+rest_vecs = [ SSA ]
 
-vecs_SA = get_super_vectors(systems, SA)
-vecs_SSA = get_super_vectors(systems, SSA)
-vecs_MMI = get_super_vectors(systems, MMI)
+
+##########################################################################################
+
+def cone_contains(outer, inner):
+    return all([ outer.contains(ray) for ray in inner.rays() ])
+
+def cones_equal(first, second):
+    return ( len(first.rays()) == len(second.rays()) and
+             all([ ray in first.rays() for ray in second.rays() ]) )
+
+##########################################################################################
+
+# subsystems of the entire system
+subsystems = [ "".join(sys) for sys in list(power_set(systems))[1:] ]
+symbols = [ sympy.symbols(subsystem.lower(), positive = True)
+            for subsystem in subsystems ]
 
 # declare a dual vector describing a quantity that is monotone under sub-system processing
-processing_monotone = e_vec(partitions, symbols)
+processing_monotone = e_vec(subsystems, symbols)
 
 # identify all positive vectors derived from the processing monotone
-positive_vecs = set( processing_monotone.relabeled(sub,sub+"Q").split()
-                     - processing_monotone
-                     for sub in primary_subsystems(systems) )
+positive_vecs = set( processing_monotone.relabeled(sys,sys+systems_ext[-1])
+                     - processing_monotone for sys in systems )
 
 # identify cones restricting the positive vectors
 positive_cones = {}
 for vec in positive_vecs:
-    positive_cones[vec] = []
+    extreme_rays = []
     restricted_vals = set([ val if val > 0 else -val for val in vec.vals ])
     for val in restricted_vals:
         key = { symbol : 0 for symbol in symbols }
         for sign in [ 1, -1 ]:
             key[val] = sign
-            positive_cones[vec].append(vec.evaluated(key))
+            extreme_rays.append(vec.evaluated(key).standard_form(systems_ext))
+    positive_cones[vec] = sage.geometry.cone.Cone(extreme_rays)
 
-print(processing_monotone)
-print("------------------------------")
-print()
-for vec in positive_vecs:
+# identify cone restricting positive dual vectors
+rest_rays = [ get_super_vectors(systems_ext, vec) for vec in rest_vecs ]
+rest_cones = [ sage.geometry.cone.Cone([ ray.standard_form(systems_ext) for ray in rays ])
+               for rays in rest_rays ]
+rest_cone = rest_cones[0]
+for jj in range(1,len(rest_cones)):
+    rest_cone = rest_cone.intersection(rest_cones[jj])
+
+for vec, cone in positive_cones.items():
     print(vec)
-    for ray in positive_cones[vec]:
-        print(" ",ray)
-    print()
+    print
+    print(cone.intersection(rest_cone).rays())
+    print
+    print
