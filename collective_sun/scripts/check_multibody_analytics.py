bd395c530021a9fe347e9b641a5c209ff585224a
Michael A. Perlin <mika.perlin@gmail.com>
2020-03-05 14:25:56 -0600

    adding code to check the translationally invariant multi-body eigenvalue problem

--- collective_sun/scripts/check_multibody_analytics.py
+++ collective_sun/scripts/check_multibody_analytics.py
@@ -6,8 +6,11 @@ import functools, operator
 
 from itertools_extension import assignments, set_diagrams
 
-max_spins = 10 # maximum number of spins
-max_dim = 2**10 # maximum dimension of Hilbert space that we want to test out
+translational_invariance = True
+
+min_spins = 3
+max_spins = 8
+max_dim = 2**max_spins
 
 total_dim = max_dim # total dimension of Hilbert space
 while total_dim >= max_dim:
@@ -29,16 +32,65 @@ print("total number of spins:", spin_num)
 print("multi-body operator dimensions", dimensions)
 
 ##########################################################################################
+# methods for translations on the lattice
+##########################################################################################
+
+# convert between integer and vector indices for a spin
+_to_vec = { idx : tuple(vec) for idx, vec in enumerate(np.ndindex(lattice_shape)) }
+_to_idx = { vec : idx for idx, vec in _to_vec.items() }
+def to_vec(idx):
+    if hasattr(idx, "__getitem__"):
+        return np.array(idx) % np.array(lattice_shape)
+    return np.array(_to_vec[ idx % spin_num ])
+def to_idx(vec):
+    if type(vec) is int:
+        return vec % spin_num
+    return _to_idx[ tuple( np.array(vec) % np.array(lattice_shape) ) ]
+
+# shift a choice of spins spins by a displacement
+def shift_spins(spins, disp, aa = None):
+    if aa is None: # shift all spins
+        return tuple( to_idx( to_vec(idx) + to_vec(disp) ) for idx in spins )
+    else: # only shift spin aa
+        new_spins = list(spins)
+        new_spins[aa] = to_idx( to_vec(spins[aa]) + to_vec(disp) )
+        return tuple(new_spins)
+
+##########################################################################################
+# basic objects for building vectors / tensors
+##########################################################################################
+
+def unit_tensor(idx, size = spin_num):
+    tensor = np.zeros((size,)*len(idx))
+    tensor[tuple( np.array(idx) % size )] = 1
+    return tensor
+
+def unit_vector(idx, size = spin_dim):
+    return unit_tensor((idx,), size)
+
+def sym_tensor(choice):
+    return sum( unit_tensor(kk) for kk in it.permutations(choice) )
+
+def sym_shift_tensor(choice):
+    tensor = np.zeros((spin_num,)*len(choice))
+    for center in range(spin_num):
+        idx = shift_spins(choice,center)
+        tensor += sym_tensor(idx)
+    tensor /= tensor.max()
+    return tensor
+
+def _remove_diags(tensor):
+    for kk in np.ndindex(tensor.shape):
+        if len(set(kk)) != len(kk):
+            tensor[kk] = 0
+    return tensor
+
+##########################################################################################
 # construct projector onto the fully symmetric manifold
 ##########################################################################################
 
 # labels for all fully symmetric states
 sym_labels = list(assignments(spin_num,spin_dim))
-
-def unit_vector(size, idx):
-    vec = np.zeros(size)
-    vec[idx] = 1
-    return vec
 
 # build a fully symmetric state labelled by occupation number
 def sym_state(occupations):
@@ -46,7 +98,7 @@ def sym_state(occupations):
     labels = [ [mm]*pop for mm, pop in enumerate(occupations) ]
     labels = np.concatenate(labels).astype(int)
     def _base_state(label):
-        return unit_vector(spin_dim, label)
+        return unit_vector(label)
     vec = sum( functools.reduce(np.kron, map(_base_state, perm))
                for perm in it.permutations(labels) )
     return vec / np.sqrt(vec @ vec)
@@ -64,18 +116,19 @@ sym_proj = sum( to_proj(sym_state(label)) for label in sym_labels )
 
 # return a random symmetric tensor with zeros on all diagonal blocks
 def random_tensor(dimension):
-    tensor = np.zeros((spin_num,)*dimension)
-    for comb in it.combinations(range(spin_num), dimension):
-        num = np.random.rand()
-        for perm in it.permutations(comb):
-            tensor[perm] = num
-    return tensor
+    if not translational_invariance:
+        return sum( np.random.rand() * sym_tensor(choice)
+                    for choice in it.combinations(range(spin_num), dimension) )
+    else:
+        return sum( np.random.rand() * sym_shift_tensor((0,)+choice)
+                    for choice in it.combinations(range(1,spin_num), dimension-1) )
 
 # random `dimension`-local operator that obeys permutational symmetry
 def random_op(dimension):
     ops = [ np.random.rand(spin_dim,spin_dim) for op in range(dimension) ]
+    perms = np.math.factorial(dimension)
     return sum( functools.reduce(np.kron, [ ops[pp] for pp in perm ])
-                for perm in it.permutations(range(dimension)) ) / np.math.factorial(dimension)
+                for perm in it.permutations(range(dimension)) ) / perms
 
 # act with the multi-local operator `op` on the spins indexed by `indices`
 def act(op, indices):
@@ -114,10 +167,13 @@ def act(op, indices):
 tensors = [ random_tensor(dimension) for dimension in dimensions ]
 base_ops = [ random_op(dimension) for dimension in dimensions ]
 
+def build_op(tensor, base_op):
+    return sum( tensor[idx] * act(base_op, idx)
+                for idx in it.combinations(range(spin_num), tensor.ndim) )
+
 # construct the full, exact product of multi-body operators
-full_ops = [ sum( tensor[idx] * act(base_op, idx)
-                  for idx in it.combinations(range(spin_num), dimension) )
-             for tensor, base_op, dimension in zip(tensors, base_ops, dimensions) ]
+full_ops = [ build_op(tensor, base_op)
+             for tensor, base_op in zip(tensors, base_ops) ]
 exact_op = sym_proj @ functools.reduce(operator.matmul, full_ops) @ sym_proj
 
 # for shared index groups of given sizes, generate all allowed values of indices
@@ -153,11 +209,11 @@ for set_diagram in set_diagrams(dimensions):
     def _indices(subset_idx_vals, primary_index):
         return restrict_indices(subset_idx_vals, set_diagram.keys(), primary_index)
 
-    diagram_coefficient = 0
+    diagram_coeficient = 0
     for idx_vals in _index_val_set():
         tensor_vals = ( tensor[_indices(idx_vals, tt)]
                         for tt, tensor in enumerate(tensors) )
-        diagram_coefficient += functools.reduce(operator.mul, tensor_vals)
+        diagram_coeficient += functools.reduce(operator.mul, tensor_vals)
 
     try:
         idx_vals = next(_index_val_set())
@@ -167,48 +223,120 @@ for set_diagram in set_diagrams(dimensions):
                       for pp, base_op in enumerate(base_ops) )
     diagram_op = functools.reduce(operator.matmul, evaluated_ops)
 
-    simp_op += diagram_coefficient * ( sym_proj @ diagram_op @ sym_proj )
+    simp_op += diagram_coeficient * ( sym_proj @ diagram_op @ sym_proj )
 
 # verify that the exact and simplified products of multi-body operators are equal
 print(np.allclose(simp_op, exact_op))
 
 ##########################################################################################
-# verify the multi-body eigenvalue problem
+# verify the diagonsis of multi-body excitations
 ##########################################################################################
 
 # build random SU(n)-symmetric interaction Hamiltonian
-sun_coeffs = random_tensor(2)
-swap = sum( ( lambda op : np.kron(op.transpose(),op) )
-            ( np.outer(unit_vector(spin_dim,nu),
-                       unit_vector(spin_dim,mu)) )
+sun_coefs = random_tensor(2)
+swap = sum( ( lambda op : np.kron(op,op.T) )( unit_tensor((mu,nu), spin_dim) )
             for mu in range(spin_dim) for nu in range(spin_dim) )
-sun_interactions = sum( sun_coeffs[pp,qq] * act(swap,[pp,qq])
+sun_interactions = sum( sun_coefs[pp,qq] * act(swap,[pp,qq])
                         for pp in range(spin_num) for qq in range(pp) )
 
 # build objects that appear in the multi-body eigenvalue problem
-sym_energy = sun_coeffs.sum()/2
-sun_coeff_vec = sum(sun_coeffs)
-def _coeff_mod(spin_choice):
-    return sum( sun_coeff_vec[pp] - sum( sun_coeffs[pp,qq] for qq in spin_choice )
-                for pp in spin_choice )
-def _coeff_zip_tensor(tensor):
+sym_energy = sun_coefs.sum()/2
+sun_coef_vec = sum(sun_coefs)
+
+def _coef_mod_val(choice):
+    return sum( sun_coef_vec[pp] - sum( sun_coefs[pp,qq] for qq in choice )
+                for pp in choice )
+def coef_mod(tensor):
+    coef_mod = np.zeros(tensor.shape)
+    for choice in it.combinations(range(spin_num), tensor.ndim):
+        for perm in it.permutations(choice):
+            coef_mod[perm] = _coef_mod_val(perm)
+    return coef_mod * tensor
+
+def coef_zip(tensor):
     axes = tensor.ndim
     def _zip_axis(axis):
-        prod = np.tensordot(sun_coeffs, tensor, axes = [ 1, axis ])
+        prod = np.tensordot(sun_coefs, tensor, axes = [ 1, axis ])
         old_order = [ axis ] + [ jj for jj in range(axes) if jj != axis ]
         new_order = np.arange(axes)[np.argsort(old_order)]
         return np.transpose(prod, axes = new_order)
-    return sum( _zip_axis(axis) for axis in range(axes) )
+    zipped_tensor = sum( _zip_axis(axis) for axis in range(axes) )
+    return _remove_diags(zipped_tensor)
+
+def coef_act(tensor):
+    return coef_zip(tensor) - coef_mod(tensor)
 
 # verify the multi-body eigenvalue problem for every multi-body operator that we built
 for tensor, base_op, full_op in zip(tensors, base_ops, full_ops):
     exact = sun_interactions @ full_op @ sym_proj
 
-    _coeff_zip = _coeff_zip_tensor(tensor)
-    remaining_op = sum( ( _coeff_zip[idx] - _coeff_mod(idx) * tensor[idx] )
-                        * act(base_op, idx)
-                        for idx in it.combinations(range(spin_num), tensor.ndim) )
+    coef_act_tensor = coef_act(tensor)
+    remaining_op = build_op(coef_act_tensor, base_op)
 
     simpl = ( sym_energy * full_op + remaining_op ) @ sym_proj
 
     print(np.allclose(simpl, exact))
+
+##########################################################################################
+# verify the multi-body eigenvalue problem
+##########################################################################################
+
+if not translational_invariance: exit()
+
+# for each allowed tensor dimension
+for dimension, base_op in zip(dimensions, base_ops):
+
+    # identify ( all choices of spins ) modulo ( translations )
+    trans_choices = {}
+    distinct_choices = 0
+    for choice in it.combinations(range(1,spin_num), dimension-1):
+        choice = (0,) + choice
+        add_to_choices = True
+        for center in range(spin_num):
+            shifted_choice = tuple(sorted(shift_spins(choice,center)))
+            if shifted_choice in trans_choices:
+                add_to_choices = False
+                break
+        if add_to_choices:
+            trans_choices[choice] = distinct_choices
+            distinct_choices += 1
+
+    # get the index of a spin choice
+    def _choice_trans_idx(choice):
+        if len(set(choice)) != len(choice): return None
+        for shift in range(spin_num):
+            shifted_choice = shift_spins(choice,shift)
+            shifted_choice = tuple(sorted(shifted_choice))
+            idx = trans_choices.get(shifted_choice)
+            if idx is not None:
+                return idx
+
+    def vector_to_tensor(vector):
+        return sum( val * sym_shift_tensor(choice)
+                    for val, choice in zip(vector, trans_choices.keys()) )
+
+    # build matrix for many-body eigenvalue problem
+    dim = len(trans_choices)
+    eig_mat = np.zeros((dim,dim))
+    for choice, kk in trans_choices.items():
+        eig_mat[kk,kk] += sum( sun_coefs[pp,qq] for pp in choice for qq in choice )
+        for dd, aa in it.product(range(spin_num),range(dimension)):
+            shifted_choice = shift_spins(choice,dd,aa)
+            idx = _choice_trans_idx(shifted_choice)
+            if idx is None: continue
+            eig_mat[kk,idx] += sun_coefs[dd,0]
+
+    # check that eigenvectors of eig_mat generate states of definite excitation energy
+    values, vectors = np.linalg.eig(eig_mat)
+    for value, vector in zip(values, vectors.T):
+        excitation_energy = value - dimension * sun_coef_vec[0]
+
+        tensor = vector_to_tensor(vector)
+        operator = build_op(tensor, base_op)
+
+        act_tensor = coef_act(tensor)
+        print(np.allclose(act_tensor, excitation_energy * tensor))
+
+        exact = sun_interactions @ operator @ sym_proj
+        simpl = ( sym_energy + excitation_energy ) * operator @ sym_proj
+        print(np.allclose(simpl, exact))
