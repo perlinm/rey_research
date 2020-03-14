689f5cdafb093f34acc35689b1cd50cbb25941a5
Michael A. Perlin <mika.perlin@gmail.com>
2020-03-13 20:25:18 -0600

    added symmetrization over reflections in multibody_problem note: multibody_methods is currently known to generally symmetrize improperly

--- collective_sun/scripts/multibody_methods.py
+++ collective_sun/scripts/multibody_methods.py
@@ -66,7 +66,7 @@ def unit_tensor(idx, size):
     tensor[tuple(idx)] = 1
     return tensor
 
-# "unit" symmetric tensor, possibly symmetrized over all translations on a lattice
+# unit symmetric tensor
 def sym_tensor(idx, size):
     dimension = len(idx)
     tensor = np.zeros((size,)*dimension, dtype = int)
@@ -74,26 +74,17 @@ def sym_tensor(idx, size):
         tensor[perm_idx] = 1
     return tensor
 
-# "unit" symmetric tensor, symmetrized over all translations on a lattice
-def sym_tensor_TI(idx, size, spin_shift = None):
+# unit symmetric tensor, symmetrized over all translations
+#    and possibly some other equivalence relation
+def sym_tensor_symmetrized(idx, size, spin_shift, get_equivalent = None):
+    if get_equivalent is None:
+        get_equivalent = lambda idx : idx
     dimension = len(idx)
     tensor = np.zeros((size,)*dimension, dtype = int)
-    for displacement in range(size):
-        shifted_idx = spin_shift(idx,displacement)
+    for shift, equivalent_idx in it.product(range(size),get_equivalent(idx)):
+        shifted_idx = spin_shift(equivalent_idx,shift)
         for perm_idx in it.permutations(shifted_idx):
             tensor[perm_idx] |= 1 # set tensor[perm_idx] to 1 (from either 0 or 1)
-    return tensor
-
-# "unit" symmetric tensor, symmetrized over all translations and reflections on a lattice
-def sym_tensor_TI_iso(idx, size, spin_shift, spin_reflect, lattice_dim):
-    dimension = len(idx)
-    tensor = np.zeros((size,)*dimension, dtype = int)
-    for reflection in it.product([1,-1], repeat = lattice_dim):
-        reflected_idx = spin_reflect(idx,reflection)
-        for displacement in range(size):
-            shifted_idx = spin_shift(reflected_idx,displacement)
-            for perm_idx in it.permutations(shifted_idx):
-                tensor[perm_idx] |= 1
     return tensor
 
 def random_tensor(dimension, lattice_shape, TI, seed = None):
@@ -101,19 +92,18 @@ def random_tensor(dimension, lattice_shape, TI, seed = None):
     spin_num = np.prod(lattice_shape)
     if TI:
         spin_shift = spin_shift_method(lattice_shape)
-        return sum( np.random.rand() * sym_tensor((0,)+choice, spin_num, spin_shift)
+        return sum( np.random.rand() *
+                    sym_tensor_symmetrized((0,)+choice, spin_num, spin_shift)
                     for choice in it.combinations(range(1,spin_num), dimension-1) )
     else:
         return sum( np.random.rand() * sym_tensor(choice, spin_num)
                     for choice in it.combinations(range(spin_num), dimension) )
 
-# TODO: symmetrize over rotations/reflections for isotropic systems
-
 ##########################################################################################
 # setting up the multibody eigenvalue problem
 ##########################################################################################
 
-def multibody_problem(lattice_shape, sun_coefs, dimension, TI = True):
+def multibody_problem(lattice_shape, sun_coefs, dimension, TI = True, isotropic = None):
     if dimension == 0:
         excitation_mat = np.zeros((1,1))
         def vector_to_tensor(vector): return vector[0] * np.ones(())
@@ -125,6 +115,8 @@ def multibody_problem(lattice_shape, sun_coefs, dimension, TI = True):
     sun_coef_vec = sum(sun_coefs)
     sun_coef_0 = sun_coef_vec[0]
     spin_shift = spin_shift_method(lattice_shape)
+    assert( not ( isotropic and not TI ) )
+    if isotropic is None: isotropic = TI
 
     def _diag_val(choice):
         return sum( sun_coefs[pp,qq] for pp in choice for qq in choice ) \
@@ -132,7 +124,7 @@ def multibody_problem(lattice_shape, sun_coefs, dimension, TI = True):
 
     # identify all distinct choices of spins,
     #   modding out by translations and rotations/reflections if appropriate
-    if not TI:
+    if not TI: # no translational invariance
         choices = { choice : idx
                     for idx, choice
                     in enumerate(it.combinations(range(spin_num), dimension)) }
@@ -147,49 +139,92 @@ def multibody_problem(lattice_shape, sun_coefs, dimension, TI = True):
         def choice_tensor(choice):
             return sym_tensor(choice, spin_num)
 
-    # translationally invariant but not isotropic systems
+        # build matrix for many-body eigenvalue problem
+        excitation_mat = np.zeros((choice_num, choice_num))
+        for choice, idx in choices.items():
+            excitation_mat[idx,idx] += _diag_val(choice)
+            for pp, aa in it.product(range(spin_num), range(dimension)):
+                choice_aa = choice[aa]
+                choice_aa_pp = list(choice); choice_aa_pp[aa] = pp
+                if len(set(choice_aa_pp)) != dimension: continue
+                choice_aa_pp_idx = get_choice_idx(choice_aa_pp)
+                excitation_mat[idx,choice_aa_pp_idx] += sun_coefs[choice_aa,pp]
+
+    # translationally invariant and maybe isotropic systems
     else:
+        # given a choice of spins, return all choices in its equivalence class
+        if not isotropic:
+            def get_equivalent(spins):
+                return ( tuple(sorted(spin_shift(spins,shift,neg=True)))
+                         for shift in spins )
+        else:
+            # get all reflections of a spin choice that are distinct up to a translation
+            spin_reflect = spin_reflect_method(lattice_shape)
+            def get_reflections(spins):
+                fixed_spins = spin_shift(spins,spins[0],neg=True)
+                reflections = []
+                for reflection in it.product([1,-1], repeat = len(lattice_shape)):
+                    ref_spins = spin_reflect(fixed_spins, reflection)
+                    add_reflection = True
+                    for shift in ref_spins:
+                        shifted_spins = spin_shift(ref_spins,shift,neg=True)
+                        shifted_spins = tuple(sorted(shifted_spins))
+                        if shifted_spins in reflections:
+                            add_reflection = False
+                            break
+                    if add_reflection:
+                        reflections += [ ref_spins ]
+                return reflections
+
+            # TODO: address remaining symmetries (after TI and reflections)
+            #       in isotropic systems
+            def get_equivalent(spins):
+                return set( tuple(sorted(spin_shift(reflected_choice,shift,neg=True)))
+                            for reflected_choice in get_reflections(spins)
+                            for shift in reflected_choice )
+
+        # construct all equivalence classes of choices of spins
         choices = {}
         choice_num = 0
         for choice in it.combinations(range(1,spin_num), dimension-1):
             choice = (0,) + choice
             add_to_choices = True
-            for shift in choice:
-                shifted_choice = tuple(sorted(spin_shift(choice,shift,neg=True)))
-                if shifted_choice in choices:
+            for equivalent_choice in get_equivalent(choice):
+                if equivalent_choice in choices:
                     add_to_choices = False
                     break
             if add_to_choices:
                 choices[choice] = choice_num
                 choice_num += 1
 
+        # get the equivalence class of a choice of spins
         def get_choice_idx(choice):
             if len(set(choice)) != len(choice): return None
-            for shift in choice:
-                shifted_choice = tuple(sorted(spin_shift(choice,shift,neg=True)))
-                idx = choices.get(shifted_choice)
+            for equivalent_choice in get_equivalent(choice):
+                idx = choices.get(equivalent_choice)
                 if idx is not None:
                     return idx
 
+        # construct an appropriately symmetrized tensor corresponding to a choice of spins
         def choice_tensor(choice):
-            return sym_tensor_TI(choice, spin_num, spin_shift)
+            return sym_tensor_symmetrized(choice, spin_num, spin_shift, get_equivalent)
 
-    # build matrix for many-body eigenvalue problem
-    excitation_mat = np.zeros((choice_num, choice_num))
-    for choice, idx in choices.items():
-        excitation_mat[idx,idx] += _diag_val(choice)
-        for pp, aa in it.product(range(spin_num), range(dimension)):
-            choice_aa = choice[aa]
-            choice_aa_pp = list(choice); choice_aa_pp[aa] = pp
-            if len(set(choice_aa_pp)) != dimension: continue
-            choice_aa_pp_idx = get_choice_idx(choice_aa_pp)
-            excitation_mat[idx,choice_aa_pp_idx] += sun_coefs[choice_aa,pp]
+        # build matrix for many-body eigenvalue problem
+        excitation_mat = np.zeros((choice_num, choice_num))
+        for choice, idx in choices.items():
+            excitation_mat[idx,idx] += _diag_val(choice)
+            for pp, aa in it.product(range(spin_num), range(dimension)):
+                choice_aa = choice[aa]
+                choice_aa_pp = list(choice); choice_aa_pp[aa] = pp
+                if len(set(choice_aa_pp)) != dimension: continue
+                choice_aa_pp_idx = get_choice_idx(choice_aa_pp)
+                excitation_mat[idx,choice_aa_pp_idx] += sun_coefs[choice_aa,pp]
 
     # build methods to convert between a tensor and a "choice vector"
     def vector_to_tensor(vector):
         return sum( val * choice_tensor(choice)
-                    for val, choice in zip(vector, choices.keys()) )
+                    for val, choice in zip(vector, choices) )
     def tensor_to_vector(tensor):
-        return np.array([ tensor[idx] for idx in choices.keys() ])
+        return np.array([ tensor[choice] for choice in choices ])
 
     return excitation_mat, vector_to_tensor, tensor_to_vector
