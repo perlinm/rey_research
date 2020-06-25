298613048a13a80210ce655e453c5d0b41afe591
Michael A. Perlin <mika.perlin@gmail.com>
2020-06-25 17:34:42 -0500

    shifted around code in plot_structure.py

--- collective_sun/scripts/plot_structure.py
+++ collective_sun/scripts/plot_structure.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 
 from multibody_methods import dist_method, get_multibody_states
 
-spin_num = 101
+spin_num = 21
 alpha = 2
 columns = 4
 shell_cutoff = 20
@@ -59,6 +59,26 @@ shell_digits = len(str(shell_num))
 # plot generators within the 2- and 3-excitation manifolds
 ##########################################################################################
 
+def get_matrix(tensor):
+    assert( tensor.ndim in [ 2, 3] )
+    if tensor.ndim == 2:
+        mat = tensor
+        sign = np.sign(mat[0,1])
+    elif tensor.ndim == 3:
+        mat = tensor[:,:,0]
+        sign = np.sign(mat[1,2])
+    mat *= sign / abs(max(mat.flatten()))
+    mat = np.roll(mat, spin_num//2, axis = 0)
+    mat = np.roll(mat, spin_num//2, axis = 1)
+    return mat
+
+def plot_matrix(axis, matrix, energy):
+    title = r"$\Delta\approx" + f"{energy:.2f}" + "$"
+    axis.set_title(title)
+    axis.imshow(matrix, **plot_args)
+    axis.set_xticks([])
+    axis.set_yticks([])
+
 # make figure showing several shells
 figure, axes = plt.subplots(2, columns, figsize = panel_figsize)
 
@@ -70,32 +90,15 @@ for manifold, row_axes in zip([ 2, 3 ], axes):
     shells = sunc["shells"][manifold]
     for idx, shell in enumerate(shells):
         if idx > shell_cutoff: break
-
         energy = sunc["energies"][shell]
-        title = r"$\Delta\approx" + f"{energy:.2f}" + "$"
-        ax.set_title(title)
-
-        if manifold == 2:
-            mat = sunc[shell]
-            sign = np.sign(mat[0,1])
-        else:
-            mat = sunc[shell][0,:,:]
-            sign = np.sign(mat[1,2])
-        mat *= sign / abs(max(mat.flatten()))
-        mat = np.roll(mat, spin_num//2, axis = 0)
-        mat = np.roll(mat, spin_num//2, axis = 1)
-        ax.imshow(mat, **plot_args)
+        matrix = get_matrix(sunc[shell])
+        plot_matrix(ax, matrix, energy)
 
         if idx < columns:
-            row_axes[idx].set_title(title)
-            row_axes[idx].imshow(mat, **plot_args)
-            row_axes[idx].set_xticks([])
-            row_axes[idx].set_yticks([])
-
-        ax.axis("off")
-        fig.tight_layout(pad = 0.1)
+            plot_matrix(row_axes[idx], matrix, energy)
 
         shell_str = str(shell).rjust(shell_digits, "0")
+        fig.tight_layout(pad = 0.1)
         fig.savefig(fig_dir + f"mat_L{spin_num}_a{alpha}_s{shell_str}.pdf")
         plt.close(fig)
 
