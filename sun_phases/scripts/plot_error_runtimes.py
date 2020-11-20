331510d013072981ffd97471f0de248f35893137
Michael A. Perlin <mika.perlin@gmail.com>
2020-11-20 11:24:54 -0600

    plot all bound/reconsruction times

--- sun_phases/scripts/plot_error_runtimes.py
+++ sun_phases/scripts/plot_error_runtimes.py
@@ -15,35 +15,36 @@ params = { "font.size" : 10,
 plt.rcParams.update(params)
 
 figsize = (3,2)
-fit_dim_min = 100
 
 ##################################################
-
-files = glob.glob(data_dir + "times_*_serial.txt")
 
 def max_dim(file):
     pieces = file.replace(".txt","").split("/")[-1].split("_")
     return pieces[-1].split("-")[-1]
-files = sorted(files, key = max_dim)
-
-data = np.vstack([ np.loadtxt(file) for file in files ])
-sizes = data[:,0]
-times = data[:,1]
+def get_data(tag):
+    files = sorted(glob.glob(data_dir + f"times_{tag}_d*.txt"), key = max_dim)
+    data = np.vstack([ np.loadtxt(file) for file in files ])
+    return data[:,0], data[:,1]
 
 plt.figure(figsize = figsize)
-plt.loglog(sizes, times, "k.", label = "data")
 
-size_lims = plt.gca().get_xlim()
-time_lims = plt.gca().get_ylim()
+for tag, label, marker in [ ( "CB", r"$\mathcal{S}_V$", "ro" ),
+                            ( "QB", r"$\epsilon_V$", "k." ),
+                            ( "RE", r"$\mathcal{E}_V(\rho)$", "b." ) ]:
+    sizes, times = get_data(tag)
+    plot = plt.loglog(sizes, times, marker, label = label)
+    color = plot[0].get_color()
 
-fit_idx = sizes >= fit_dim_min
-fit = np.polyfit(np.log(sizes[fit_idx]), np.log(times[fit_idx]), deg = 1)
-plt.loglog(size_lims, np.exp(fit[0]*np.log(size_lims) + fit[1]), "k--", label = "fit")
-plt.gca().set_xlim(size_lims)
-plt.gca().set_ylim(time_lims)
+    size_lims = plt.gca().get_xlim()
+    time_lims = plt.gca().get_ylim()
+    fit_idx = sizes >= sizes[-1]//2
+    fit = np.polyfit(np.log(sizes[fit_idx]), np.log(times[fit_idx]), deg = 1)
+    plt.loglog(size_lims, np.exp(fit[0]*np.log(size_lims) + fit[1]), "--", color = color)
+    plt.gca().set_xlim(size_lims)
+    plt.gca().set_ylim(time_lims)
 
-plt.xlabel(r"qudit dimension $d$")
-plt.ylabel(r"seconds to compute $\mathcal{S}_V$")
+plt.xlabel(r"qudit dimension $(d)$")
+plt.ylabel(r"seconds $(t)$")
 plt.legend(loc = "best")
 plt.tight_layout()
-plt.savefig(fig_dir + "error_times.pdf")
+plt.savefig(fig_dir + "qudit_times.pdf")
