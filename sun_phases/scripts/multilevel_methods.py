f02ba8d0659380e5538a73ca52d91103ac87c0a6
Michael A. Perlin <mika.perlin@gmail.com>
2021-01-07 11:31:37 -0600

    renaming in multilevel_methods.py

--- sun_phases/scripts/multilevel_methods.py
+++ sun_phases/scripts/multilevel_methods.py
@@ -6,7 +6,7 @@ from sympy.physics.quantum.cg import CG as sym_cg
 from sympy.physics.wigner import wigner_6j as sym_wigner_6j
 
 # transition and drive operators, exact
-def transition_op_exact(dim, L, M):
+def trans_op_exact(dim, L, M):
     if L >= dim or abs(M) > L: return np.zeros((dim,dim))
     L, M = sym.S(L), sym.S(M)
     I = sym.S(dim-1)/2
@@ -18,14 +18,14 @@ def transition_op_exact(dim, L, M):
 
 def drive_op_exact(dim, L, M):
     if L >= dim or abs(M) > L: return np.zeros((dim,dim))
-    T_LM = transition_op_exact(dim, L, M)
+    T_LM = trans_op_exact(dim, L, M)
     if M == 0: return T_LM
     eta_M = (-1)**M if M > 0 else sym.I
     return eta_M/sym.sqrt(2) * ( T_LM + np.sign(M) * T_LM.conj().T )
 
 # transition and drive operators, numerical
-def transition_op(dim, L, M):
-    return transition_op_exact(dim, L, M).astype(float)
+def trans_op(dim, L, M):
+    return trans_op_exact(dim, L, M).astype(float)
 
 def drive_op(dim, L, M):
     dtype = float if M >= 0 else complex
@@ -41,11 +41,11 @@ def drive_scale(dim, L):
     return float(drive_scale_exact(dim, L))
 
 # transition product expansion coefficient (structure factor)
-def transition_prod_coef_exact(dim, l1, m1, l2, m2, L, M):
+def trans_prod_coef_exact(dim, l1, m1, l2, m2, L, M):
     I = sym.S(dim-1)/2
     return ( (-1)**(2*I+L) * sym.sqrt((2*l1+1)*(2*l2+1)) *
              sym_cg(l1, m1, l2, m2, L, M).doit() *
              sym_wigner_6j(l1, l2, L, I, I, I) )
 
-def transition_prod_coef(dim, l1, m1, l2, m2, L, M):
-    return float(transition_prod_coef_exact(dim, l1, m1, l2, m2, L, M))
+def trans_prod_coef(dim, l1, m1, l2, m2, L, M):
+    return float(trans_prod_coef_exact(dim, l1, m1, l2, m2, L, M))
