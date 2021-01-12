edb5309efdec89f8e1b4ae879a1698068e31b134
Michael A. Perlin <mika.perlin@gmail.com>
2021-01-12 13:47:32 -0600

    force print updates

--- sun_phases/scripts/plot_spin_pair.py
+++ sun_phases/scripts/plot_spin_pair.py
@@ -60,6 +60,7 @@ LTA_info = {}
 
 for field_idx, ( field, file ) in enumerate(sorted(zip(fields,files))):
     print(f"{field_idx}/{len(fields)}")
+    sys.stdout.flush()
 
     data = np.loadtxt(file, dtype = complex)
     times, states_0, states_1 = data[:,0], data[:,1:dim+1], data[:,dim+1:]
