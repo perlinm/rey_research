4fcbe1eb808fe71e615cd42403550b3e63b0c8b3
Michael A. Perlin <mika.perlin@gmail.com>
2021-03-25 15:53:09 -0500

    plot scaling of for lim_{h->0} <<sx>> with spin dimension n

--- sun_phases/scripts/plot_boson_states.py
+++ sun_phases/scripts/plot_boson_states.py
@@ -15,9 +15,6 @@ spin_num = int(sys.argv[2])
 
 assert( init_state_str in [ "X" ] )
 
-figsize = (3,2)
-big_figsize = (3,3.5)
-
 data_dir = "../data/spin_bosons/"
 fig_dir = "../spin_model_paper/figures/"
 def sys_tag(spin_dim):
@@ -26,6 +23,8 @@ def sys_tag(spin_dim):
 fontsize = 9
 preamble = r"""
 \usepackage{braket,bm}
+\newcommand{\MFT}{\mathrm{MFT}}
+\newcommand{\bbk}[1]{\langle\!\langle #1 \rangle\!\rangle}
 """
 params = { "font.family" : "serif",
            "font.serif" : "Computer Modern",
@@ -35,9 +34,7 @@ params = { "font.family" : "serif",
            "text.usetex" : True,
            "text.latex.preamble" : preamble }
 plt.rcParams.update(params)
-
-def op_text(text):
-    return r"$\braket{\!\braket{" + text + r"}\!}_{\mathrm{MFT}}$"
+markers = [ "o", "h", "p", "s", "^" ]
 
 ##########################################################################################
 
@@ -57,20 +54,21 @@ def vals_to_states(vals, dim):
     return states
 
 inset = mpl_toolkits.axes_grid1.inset_locator.inset_axes
-figure, axes = plt.subplots(2, figsize = big_figsize, sharex = True, sharey = True)
+figure, axes = plt.subplots(2, figsize = (2.6,3), sharex = True, sharey = True)
 sub_axes = [ inset(axes[0], "35%", "35%", loc = "upper right"),
              inset(axes[1], "35%", "35%", loc = "upper right") ]
 
 files = glob.glob(data_dir + f"mean_state_{sys_tag('*')}*")
 dims = np.array(sorted(set([ get_info(file)["dim"] for file in files ])))
 log10_crits = np.zeros(dims.size)
-log10_errs = np.zeros(dims.size)
+mean_x_max = np.zeros(dims.size)
 
 for dim_idx, dim in enumerate(dims):
     log10_fields, dim_files \
         = zip(*sorted([ ( get_info(file)["log10_field"], file )
                         for file in files if get_info(file)["dim"] == dim ]))
     log10_fields = np.array(log10_fields)
+    log10_fields_reduced = log10_fields + np.log10((dim/2)**(1/3))
 
     # collect long-time averages
     spin = (dim-1)/2
@@ -94,22 +92,27 @@ for dim_idx, dim in enumerate(dims):
     indices = slice(zero_start-3, zero_start)
     fit = np.polyfit(log10_fields[indices], mean_x[indices], 2)
     log10_crits[dim_idx] = min(np.roots(fit)[-1], log10_fields[zero_start])
-    log10_errs[dim_idx] = log10_fields[zero_start] - log10_fields[zero_start-1]
+
+    # determine \lim_{h->0} <<sx>>
+    if dim == 2:
+        log10_fields_2 = log10_fields
+        mean_x_2 = mean_x
+    equiv_mean_x_2 = np.interp(log10_fields_reduced[0], log10_fields_2, mean_x_2)
+    mean_x_max[dim_idx] = mean_x[0] / equiv_mean_x_2
 
     # plot main data
-    axes[0].plot(log10_fields, mean_x, "o", label = dim, zorder = -dim)
-    axes[1].plot(log10_fields, mean_ss, "o", label = dim, zorder = -dim)
+    axes[0].plot(log10_fields, mean_x, markers[dim_idx], label = dim, zorder = -dim)
+    axes[1].plot(log10_fields, mean_ss, markers[dim_idx], label = dim, zorder = -dim)
 
     # plot insets
-    reduced_fields = log10_fields + np.log10((dim/2)**(1/3))
     fac_x = (dim-1)**0.6
     fac_ss = np.sqrt(np.pi) * scipy.special.gamma(dim) / scipy.special.gamma(dim-1/2)
-    sub_axes[0].plot(reduced_fields, mean_x*fac_x, ".", label = dim, zorder = -dim)
-    sub_axes[1].plot(reduced_fields, mean_ss*fac_ss-1, ".", label = dim, zorder = -dim)
+    sub_axes[0].plot(log10_fields_reduced, mean_x*fac_x, ".", label = dim, zorder = -dim)
+    sub_axes[1].plot(log10_fields_reduced, mean_ss*fac_ss-1, ".", label = dim, zorder = -dim)
 
 # label axes and set axis ticks
-axes[0].set_ylabel(op_text(r"\bar\sigma_{\mathrm{x}}"))
-axes[1].set_ylabel(op_text(r"\bar{\bm s}\cdot\bar{\bm s}"))
+axes[0].set_ylabel(r"$\bbk{\bar\sigma_{\mathrm{x}}}_\MFT$")
+axes[1].set_ylabel(r"$\bbk{\bar{\bm s}\cdot\bar{\bm s}}_\MFT$")
 axes[1].set_xlabel(r"$\log_{10}(J\phi/U)$")
 for axis in sub_axes:
     axis.set_xticks([])
@@ -117,31 +120,36 @@ for axis in sub_axes:
 
 # make legend and save figure
 handles, labels = axes[0].get_legend_handles_labels()
-legend = figure.legend(handles, labels, loc = "lower center",
-                       bbox_to_anchor = (0.5,0.88),
-                       handletextpad = 0.1,
-                       ncol = dims.size, columnspacing = 0.5)
+legend = axes[0].legend(handles, labels, loc = "lower center",
+                        bbox_to_anchor = (0.5,0.98),
+                        handletextpad = 0.1,
+                        ncol = dims.size, columnspacing = 0.4)
 plt.subplots_adjust(hspace = 0.08)
 kwargs = dict( bbox_extra_artists = (legend,), bbox_inches = "tight" )
 plt.savefig(fig_dir + "mean_x_ss.pdf", **kwargs)
 
 ##################################################
 
+figure, axes = plt.subplots(2, figsize = (2.4,2.8), sharex = True)
+
 crits = 10**log10_crits
-crits_min_max = 10**np.array([ log10_crits - log10_errs,
-                               log10_crits + log10_errs ])
-crit_errs = abs(crits - crits_min_max)
-
-def fun(nn, aa): return (nn/2)**aa
+def fun(dim, aa): return (dim/2)**(-aa)
 popt, pcov = scipy.optimize.curve_fit(fun, dims, crits)
+print(popt, np.sqrt(pcov))
+axes[0].plot(dims, crits, "ko", label = "MFT")
+axes[0].plot(dims, fun(dims, *popt), "r.", label = r"fit:~$(n/2)^\alpha$")
 
-plt.figure(figsize = figsize)
-plt.errorbar(dims, crits, yerr = crit_errs, fmt = "ko", zorder = -1, label = "MFT")
-plt.plot(dims, fun(dims, *popt), "r.", label = r"fit: $(n/2)^\alpha$")
-plt.xlabel(r"spin dimension $n$")
-plt.ylabel(r"$(J\phi/U)_{\mathrm{crit}}$")
+def fun(dim, bb): return (dim-1)**(-bb)
+popt, pcov = scipy.optimize.curve_fit(fun, dims, mean_x_max)
+print(popt, np.sqrt(pcov))
+axes[1].plot(dims, mean_x_max, "ko", label = "MFT")
+axes[1].plot(dims, fun(dims, *popt), "r.", label = r"fit:~$(2s)^\beta$")
 
-handles, labels = plt.gca().get_legend_handles_labels()
-plt.legend(handles[::-1], labels[::-1], loc = "best")
-plt.tight_layout()
-plt.savefig(fig_dir + "crit_fields.pdf")
+axes[0].set_ylabel(r"$(J\phi/U)_{\mathrm{crit}}$")
+axes[1].set_ylabel(r"$\bbk{\bar\sigma_{\mathrm{x}}}_\MFT^0$")
+axes[1].set_xlabel(r"spin dimension $n$")
+axes[1].set_xticks(dims)
+
+for axis in axes: axis.legend(loc = "best")
+plt.subplots_adjust(hspace = 0.08)
+plt.savefig(fig_dir + "crit_fields.pdf", bbox_inches = "tight")
